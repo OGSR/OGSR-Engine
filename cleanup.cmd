09f7117ee1d4340b9da97f2bf79a37f249896502
KRodin <stalker.moderator@gmail.com>
2017-10-08 21:38:34 +0300

    Перенастроил сборку ещё раз
    Убрал ненужное копирование файлов в PostBuildEvent - пусть сразу
    собираются куда надо.
    Поправил предупреждения в некоторых проектах.

--- cleanup.cmd
+++ cleanup.cmd
@@ -4,8 +4,8 @@ RD /s /q 3rd_party\_TEMP
 RD /s /q ogsr_engine\_BIN
 RD /s /q ogsr_engine\_LIB
 RD /s /q ogsr_engine\_TEMP
-RD /s /q ogsr_engine\ipch
-del ogsr_engine\*.VC.*
-RD /s /q ogsr_package\
 RD /s /q ogsr_engine\LuaJIT\bin
+RD /s /q ipch\
+RD /s /q bin\
+del *.VC.*
 pause
