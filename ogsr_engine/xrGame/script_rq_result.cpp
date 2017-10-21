9d35bd92fddc2a7cdc3566b0d006f346a8e4fbaf
Denis Shaposhnikov <dsh@bamus.cz>
2017-10-21 14:44:13 +0200

    Добавил в script_rq_result свойство "result", которое содержит результат выполнения RayPick()
    Т.е. было пересчение с чем-нибудь или нет. RayPick() возвращает
    element >= 0, но делать такую проверку в скриптах мне не кажется
    правильным.
    
    Таким образом, в скриптах нужно делать что-то вроде
    
    local rq = level.ray_query( ... )
    if rq.result then -- что-то нашли
      local dist    = rq.range
      local obj     = rq.object
      local element = rq.element
    end

--- ogsr_engine/xrGame/script_rq_result.cpp
+++ ogsr_engine/xrGame/script_rq_result.cpp
@@ -6,8 +6,9 @@
 
 void script_rq_result::set_result(collide::rq_result _res)
 {
-	range = _res.range;
-	element = _res.element;
+	range		= _res.range;
+	element		= _res.element;
+	result		= true;
 	if (_res.O)
 	{
 		CGameObject *obj = smart_cast<CGameObject *>(_res.O);
@@ -26,5 +27,6 @@ void script_rq_result::script_register(lua_State *L)
 				.def_readonly("range", &script_rq_result::range)
 				.def_readonly("object", &script_rq_result::object)
 				.def_readonly("element", &script_rq_result::element)
+				.def_readonly("result", &script_rq_result::result)
 		];
 }
