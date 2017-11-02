4e4b0d704313017f2da35331c3e06f5e6283a3ab
Denis Shaposhnikov <dsh@bamus.cz>
2017-11-02 17:26:45 +0100

    Добавил совместимость со старыми сейвами
    Совместимость активируется только при использовании NEW_WPN_SLOTS. В
    этом случае, c помощью ai().get_alife()->header().version()
    проверяется, на какой версии был сделан сейв. Если он сделан на версии
    4 или выше, значит это сейв уже нового движка и слот предмета будет
    сохраняться/читаться только, если этот предмет находится в слоте. Если
    он не в слоте, то и нет смысла сохранять информацию о номере
    слота. Если же сейв сделан на версии < 4, значит он сделан на старом
    движке, где информация о слоте предметов не записывалась в сейв. В
    качестве номера версии используется ALIFE_VERSION, определенная в
    COMMON_AI/alife_space.h. Я увеличил ее с 3 на 4. Это версия
    записывается в сейв и читается из него, но проверяется только на
    предмет >= 2, т.ч. ее можно использовать для пометки каких-то
    изменений, ломающих совместимость.

--- ogsr_engine/COMMON_AI/alife_space.h
+++ ogsr_engine/COMMON_AI/alife_space.h
@@ -10,7 +10,7 @@
 #define XRAY_ALIFE_SPACE
 
 // ALife objects, events and tasks
-#define ALIFE_VERSION				0x0003
+#define ALIFE_VERSION				0x0004
 #define ALIFE_CHUNK_DATA			0x0000
 #define SPAWN_CHUNK_DATA			0x0001
 #define OBJECT_CHUNK_DATA			0x0002
