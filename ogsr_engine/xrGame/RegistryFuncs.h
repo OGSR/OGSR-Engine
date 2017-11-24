38f2129ffe0d702e687678e5aaaa69688834caac
KRodin <stalker.moderator@gmail.com>
2017-11-24 20:09:35 +0300

    Удалил часть файлов мультиплеера

--- ogsr_engine/xrGame/RegistryFuncs.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-void	ReadRegistry_StrValue	(LPCSTR rKeyName, char* value );
-void	WriteRegistry_StrValue	(LPCSTR rKeyName, const char* value );
-
-void	ReadRegistry_DWValue	(LPCSTR rKeyName, DWORD& value );
-void	WriteRegistry_DWValue	(LPCSTR rKeyName, const DWORD& value );
