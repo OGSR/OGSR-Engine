c1270d7c856acce8a9c077c0369d1674e322407b
Denis Shaposhnikov <dsh@bamus.cz>
2017-12-20 16:29:39 +0100

    Что бы приклады не залазили в камеру
    Взято отсюда
    https://github.com/revolucas/CoC-Xray/commit/cdb1b5c34a53997789df903d54b982c4a2eca6aa
    
    По идее, этот как раз исправляет ситуацию, когда при прицеливании
    приклад оружия как бы проникает в камеру и обрезается, что приводит к
    показыванию внутренностей приклада. Вроде бы действительно
    исправляет. Эксперимент с
    
    fov 75
    hud_fov 0.4
    
    показывает, что приклад ТОЗ-34 из НА больше не обрезается. До этого
    изменения, при тех же fov и hud_fov, было видно обрезание приклада.

--- ogsr_engine/xr_3da/device.h
+++ ogsr_engine/xr_3da/device.h
@@ -17,7 +17,7 @@ class	ENGINE_API	CGammaControl;
 #include "shader.h"
 #include "R_Backend.h"
 
-#define VIEWPORT_NEAR  0.2f
+#define VIEWPORT_NEAR  0.1f
 
 #define DEVICE_RESET_PRECACHE_FRAME_COUNT 10
 
