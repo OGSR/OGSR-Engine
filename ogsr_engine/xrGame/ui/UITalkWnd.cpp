68cb409027881173d576d63070c364d21f6eae80
Denis Shaposhnikov <dsh@bamus.cz>
2018-01-01 12:10:57 +0100

    Добавил акселлераторы к ответам в диалогах, как в ЗП
    Каждой фразе назначается акселлератор от 1 до 0. Т.е. если нажать
    соотв. кнопку - будет выбрана соответствующая фраза. Что бы эти номера
    показывались перед фразами, нужно определить дефайн
    SHOW_DIALOG_NUMBERS, т.к. для этого требуется дополнительный статик
    "num_text" в talk.xml, как в ЗП. Вот рабочий пример:
    
    <question_item x="0" y="0" width="527" height="20" min_height="13">
    	<num_text x="0" y="0" width="8" height="20">
    		<text font="letterica18" r="255" g="232" b="208" align="l" />
    	</num_text>
    	<content_text x="15" y="0" width="512" height="20">
    	<text font="letterica18" r="255" g="232" b="208" align="l" complex_mode="1"/>
    	<text_color>
    		<t r="231" g="168" b="12"/> <e r="255" g="232" b="208"/> <h r="255" g="255" b="255"/>
    	</text_color>
    	
    	</content_text>
    </question_item>
    
    
    Взято отсюда https://github.com/revolucas/CoC-Xray

--- ogsr_engine/xrGame/ui/UITalkWnd.cpp
+++ ogsr_engine/xrGame/ui/UITalkWnd.cpp
@@ -131,7 +131,7 @@ void CUITalkWnd::UpdateQuestions()
 		for(u32 i=0; i< m_pOurDialogManager->AvailableDialogs().size(); ++i)
 		{
 			const DIALOG_SHARED_PTR& phrase_dialog = m_pOurDialogManager->AvailableDialogs()[i];
-			AddQuestion(phrase_dialog->DialogCaption(), phrase_dialog->GetDialogID());
+			AddQuestion(phrase_dialog->DialogCaption(), phrase_dialog->GetDialogID(), i);
 		}
 	}
 	else
@@ -148,12 +148,13 @@ void CUITalkWnd::UpdateQuestions()
 			//выбор доступных фраз из активного диалога
 			if( m_pCurrentDialog && !m_pCurrentDialog->allIsDummy() )
 			{			
+				int number = 0;
 				for(PHRASE_VECTOR::const_iterator   it = m_pCurrentDialog->PhraseList().begin();
 					it != m_pCurrentDialog->PhraseList().end();
-					it++)
+					it++, ++number)
 				{
 					CPhrase* phrase = *it;
-					AddQuestion(phrase->GetText(), phrase->GetID());
+					AddQuestion(phrase->GetText(), phrase->GetID(), number);
 				}
 			}
 			else
@@ -335,10 +336,10 @@ void CUITalkWnd::SayPhrase(const shared_str& phrase_id)
 
 //////////////////////////////////////////////////////////////////////////
 
-void CUITalkWnd::AddQuestion(const shared_str& text, const shared_str& value)
+void CUITalkWnd::AddQuestion(const shared_str& text, const shared_str& value, int i)
 {
 	if(text.size() == 0) return;
-	UITalkDialogWnd->AddQuestion(*CStringTable().translate(text),value.c_str());
+	UITalkDialogWnd->AddQuestion(*CStringTable().translate(text),value.c_str(), i);
 }
 
 //////////////////////////////////////////////////////////////////////////
