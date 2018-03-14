643dae154cc0fcca8f8aa9ce1172d84ef034181e
KRodin <stalker.moderator@gmail.com>
2018-03-14 13:00:51 +0300

    Не знаю, надо ли тут вылетать, пока просто в лог напишем
    Чтоб было понятно, из-за чего словили исключение

--- ogsr_engine/xrGame/script_game_object_impl.h
+++ ogsr_engine/xrGame/script_game_object_impl.h
@@ -17,10 +17,9 @@ IC	CGameObject &CScriptGameObject::object	() const
 	if (m_game_object && m_game_object->lua_game_object() == this)
 		return	(*m_game_object);
 
-#ifdef DEBUG
-	ai().script_engine().script_log(eLuaMessageTypeError,"you are trying to use a destroyed object [%x]",m_game_object);
-	THROW2	(m_game_object && m_game_object->lua_game_object() == this,"Probably, you are trying to use a destroyed object!");
-#endif
+	Msg("!!You are trying to use a destroyed object [%x]", m_game_object);
+	//THROW2	(m_game_object && m_game_object->lua_game_object() == this,"Probably, you are trying to use a destroyed object!");
+
 	return	(*m_game_object);
 }
 
