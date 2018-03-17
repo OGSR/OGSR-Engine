74b64869b1c8b5e45a4f9b7a803591fe260a528f
Denis Shaposhnikov <dsh@bamus.cz>
2018-03-17 12:12:38 +0100

    Добавил obj:set_home(), который принимает CPatrolPath, вместо имени пути
    Остальные параметры у него те же самые.
    
    Добавил метод at_home(), который возвращает true/false, если моб
    находится в пределах max_radius, и at_home( pos ), который проверяет,
    что pos находится в пределах max_radius.

--- ogsr_engine/xrGame/script_game_object.h
+++ ogsr_engine/xrGame/script_game_object.h
@@ -15,6 +15,7 @@
 #include "..\xr_3da\CameraBase.h"
 #include "WeaponHUD.h"
 #include "ui/UIStatic.h"
+#include "../COMMON_AI/PATH/patrol_path.h"
 
 #include "gameobject.h"
 #include "ai_space.h"
@@ -244,6 +245,9 @@ public:
 	// CBaseMonster
 			void				skip_transfer_enemy		(bool val);
 			void				set_home				(LPCSTR name, float r_min, float r_max, bool aggressive);
+			void set_home( CPatrolPath*, float, float, bool );
+			bool at_home();
+			bool at_home( Fvector );
 			void				remove_home				();
 			void				berserk					();
 			void				set_custom_panic_threshold	(float value);
