c58367e5fdca106784c81588b2ac583c32ea9fe8
KRodin <stalker.moderator@gmail.com>
2018-04-05 22:50:19 +0300

    Добавил исходники OpenAL Soft
    Взял версию с этого коммита
    https://github.com/kcat/openal-soft/commit/414b56edec5441211dc924fef365c54267c04f1c
    В будущем попытаюсь разобраться с пропажей звука.

---
+++ 3rd_party/Src/OpenAL_Soft/build/version.h
@@ -0,0 +1,8 @@
+/* Define to the library version */
+#define ALSOFT_VERSION "1.18.2"
+
+/* Define the branch being built */
+#define ALSOFT_GIT_BRANCH "UNKNOWN"
+
+/* Define the hash of the head commit */
+#define ALSOFT_GIT_COMMIT_HASH "unknown"
