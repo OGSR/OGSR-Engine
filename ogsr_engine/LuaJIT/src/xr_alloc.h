c6befd66202e188ea992e8dc67907f6cbcfa39cc
Sergii Tkachenko <Sergii_Tkachenko@epam.com>
2018-05-18 00:37:56 +0300

    Fixed build of xr_alloc for x64 and x86 (disabled)

--- ogsr_engine/LuaJIT/src/xr_alloc.h
+++ ogsr_engine/LuaJIT/src/xr_alloc.h
@@ -1,6 +1,14 @@
 #ifndef _XR_POOL_H
 #define _XR_POOL_H
 
+#include <stddef.h>
+#include <windows.h>
+
+typedef long(*PNTAVM)(HANDLE handle, void **addr, ULONG zbits,
+    size_t *size, ULONG alloctype, ULONG prot);
+
+extern PNTAVM ntavm;
+
 // В сталкере x64 беда с luajit`ом - из-за того, что луаджит непременно требуется память из младших адресов,
 // на больших локациях луаджит не может выделить память, так как она уже занята под другие ресурсы игры.
 // Как вариант попробую выделить большой кусок памяти(128МБ) в начале игры и буду его потихоньку выдавать луаджиту
