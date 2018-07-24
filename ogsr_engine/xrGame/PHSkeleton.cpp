46da8b504c216d09594f76ef5f9cb15161061913
Denis Shaposhnikov <dsh@bamus.cz>
2018-07-24 18:08:01 +0200

    Поправил предыдущее изменение
    См. https://github.com/KRodinn/OGSR-Engine/commit/6d29c1b435551e309a93dbafb442c25d69cd8708
    
    Убрал бессмысленное условие
    
    -			if (bone>=obj->PHGetSyncItemsNumber())
    
    т.к. при наличии
    
    	if( saved_bones.size() == obj->PHGetSyncItemsNumber() )
    
    оно никогда не выполнится.

--- ogsr_engine/xrGame/PHSkeleton.cpp
+++ ogsr_engine/xrGame/PHSkeleton.cpp
@@ -247,17 +247,13 @@ void CPHSkeleton::RestoreNetState(CSE_PHSkeleton* po)
 		u16 bone = 0;
 		std::all_of(saved_bones.begin(), saved_bones.end(),[&](SPHNetState state)
 		{
-			if (bone>=obj->PHGetSyncItemsNumber())
-			{
-				Msg("~ WARNING [%s] has different state in saved_bones[%d] PHGetSyncItemsNumber[%d] Visual[%s] alive[%s]",
-					obj->Name_script(), saved_bones.size(), obj->PHGetSyncItemsNumber(), obj->cNameVisual().c_str(),obj->GetHealth()>0 ?"yes":"no");
-				return false;
-			}
 			obj->PHGetSyncItem(bone)->set_State(state);
 			bone++;
 			return true;
 		});
 	}
+        else
+          Msg( "~ [%s]: [%s] has different state in saved_bones[%u] PHGetSyncItemsNumber[%u] Visual[%s] alive[%s]", __FUNCTION__, obj->Name_script(), saved_bones.size(), obj->PHGetSyncItemsNumber(), obj->cNameVisual().c_str(), obj->GetHealth()>0 ? "yes" : "no" );
 	saved_bones.clear();
 	po->_flags.set(CSE_PHSkeleton::flSavedData,FALSE);
 	m_flags.set(CSE_PHSkeleton::flSavedData,FALSE);
