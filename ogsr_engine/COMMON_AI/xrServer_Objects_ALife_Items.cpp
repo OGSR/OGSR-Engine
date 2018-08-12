8a348cbfea4cb688a6dd04b72f1aff94507c74b0
Sergii T <joye.ramone@gmail.com>
2018-08-13 00:25:08 +0300

    Still need to check (!P.r_eof()) even in STATE_Read for items from all.spawn

--- ogsr_engine/COMMON_AI/xrServer_Objects_ALife_Items.cpp
+++ ogsr_engine/COMMON_AI/xrServer_Objects_ALife_Items.cpp
@@ -591,7 +591,9 @@ void CSE_ALifeItemWeaponMagazined::STATE_Read		(NET_Packet& P, u16 size)
 {
 	inherited::STATE_Read(P, size);
 
-	m_u8CurFireMode = P.r_u8();
+	if (!P.r_eof()) {
+		m_u8CurFireMode = P.r_u8();
+	}
 }
 void CSE_ALifeItemWeaponMagazined::STATE_Write		(NET_Packet& P)
 {
@@ -648,9 +650,11 @@ void CSE_ALifeItemWeaponMagazinedWGL::STATE_Read		(NET_Packet& P, u16 size)
 {
 	inherited::STATE_Read(P, size);
 
-	ammo_type2 = P.r_u8();
-	a_elapsed2 = P.r_u16();
-	//Msg( "~~[%s][%s] update_read: m_bGrenadeMode: [%u], iAmmoElapsed2: [%u], m_ammoType2: [%u]", __FUNCTION__, this->name(), m_bGrenadeMode, a_elapsed2, ammo_type2 );
+	if (!P.r_eof()) {
+		ammo_type2 = P.r_u8();
+		a_elapsed2 = P.r_u16();
+		//Msg( "~~[%s][%s] update_read: m_bGrenadeMode: [%u], iAmmoElapsed2: [%u], m_ammoType2: [%u]", __FUNCTION__, this->name(), m_bGrenadeMode, a_elapsed2, ammo_type2 );
+	}
 }
 
 void CSE_ALifeItemWeaponMagazinedWGL::STATE_Write		(NET_Packet& P)
