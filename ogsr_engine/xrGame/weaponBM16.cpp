5fe4f67ba7032a2f00c768e64b65a2c774f80be2
Sergii T <joye.ramone@gmail.com>
2018-08-18 15:26:55 +0300

    Fixed bm16 animation to better support vaniall configs and models Added missing animations from COP: anim_shoot_2, anim_draw_1-2, anim_holster_1-2

--- ogsr_engine/xrGame/weaponBM16.cpp
+++ ogsr_engine/xrGame/weaponBM16.cpp
@@ -12,16 +12,39 @@ void CWeaponBM16::Load	(LPCSTR section)
 	inherited::Load		(section);
 
 	animGetEx( mhud_reload1,      "anim_reload_1" );
+
 	animGetEx( mhud_shot1,        "anim_shoot_1" );
+	animGetEx( mhud_shot2,	pSettings->line_exist(hud_sect.c_str(), "anim_shoot_2") ? "anim_shoot_2"
+		: "anim_shoot");
+
 	animGetEx( mhud_idle1,        "anim_idle_1" );
 	animGetEx( mhud_idle2,        "anim_idle_2" );
-	animGetEx( mhud_zoomed_idle1, "anim_zoomed_idle_1" );
-	animGetEx( mhud_zoomed_idle2,  "anim_zoomedidle_2" );
 
-	animGetEx( mhud_idle_sprint_1, pSettings->line_exist( hud_sect.c_str(), "anim_idle_sprint_1" ) ? "anim_idle_sprint_1" : pSettings->line_exist( hud_sect.c_str(), "anim_idle_sprint" ) ? "anim_idle_sprint" : "anim_idle" );
-	animGetEx( mhud_idle_sprint_2, pSettings->line_exist( hud_sect.c_str(), "anim_idle_sprint_2" ) ? "anim_idle_sprint_2" : pSettings->line_exist( hud_sect.c_str(), "anim_idle_sprint" ) ? "anim_idle_sprint" : "anim_idle" );
-	animGetEx( mhud_idle_moving_1, pSettings->line_exist( hud_sect.c_str(), "anim_idle_moving_1" ) ? "anim_idle_moving_1" : pSettings->line_exist( hud_sect.c_str(), "anim_idle_moving" ) ? "anim_idle_moving" : "anim_idle" );
-	animGetEx( mhud_idle_moving_2, pSettings->line_exist( hud_sect.c_str(), "anim_idle_moving_2" ) ? "anim_idle_moving_2" : pSettings->line_exist( hud_sect.c_str(), "anim_idle_moving" ) ? "anim_idle_moving" : "anim_idle" );
+	animGetEx( mhud_zoomed_idle1, "anim_zoomed_idle_1" );
+
+	// что бы можно было нормальные имена в конфигах юзать
+	if (pSettings->line_exist(hud_sect.c_str(), "anim_zoomed_idle_2"))
+		animGetEx( mhud_zoomed_idle2, "anim_zoomed_idle_2" );
+	else
+		animGetEx(mhud_zoomed_idle2, "anim_zoomedidle_2");
+
+	animGetEx( mhud_idle_sprint_1, pSettings->line_exist( hud_sect.c_str(), "anim_idle_sprint_1" ) ? "anim_idle_sprint_1" 
+		: "anim_idle_1" );
+	animGetEx( mhud_idle_sprint_2, pSettings->line_exist( hud_sect.c_str(), "anim_idle_sprint_2" ) ? "anim_idle_sprint_2" 
+		: "anim_idle_2" );
+	animGetEx( mhud_idle_moving_1, pSettings->line_exist( hud_sect.c_str(), "anim_idle_moving_1" ) ? "anim_idle_moving_1" 
+		: "anim_idle_1" );
+	animGetEx( mhud_idle_moving_2, pSettings->line_exist( hud_sect.c_str(), "anim_idle_moving_2" ) ? "anim_idle_moving_2" 
+		: "anim_idle_2" );
+
+	animGetEx(mhud_show1, pSettings->line_exist(hud_sect.c_str(), "anim_draw_1") ? "anim_draw_1"
+		: "anim_draw");
+	animGetEx(mhud_show2, pSettings->line_exist(hud_sect.c_str(), "anim_draw_2") ? "anim_draw_2"
+		: "anim_draw");
+	animGetEx(mhud_hide1, pSettings->line_exist(hud_sect.c_str(), "anim_holster_1") ? "anim_holster_1"
+		: "anim_holster");
+	animGetEx(mhud_hide2, pSettings->line_exist(hud_sect.c_str(), "anim_holster_2") ? "anim_holster_2"
+		: "anim_holster");
 
 	HUD_SOUND::LoadSound(section, "snd_reload_1", m_sndReload1, m_eSoundShot);
 }
@@ -34,10 +57,12 @@ void CWeaponBM16::PlayReloadSound()
 
 void CWeaponBM16::PlayAnimShoot()
 {
-	if(m_magazine.size()==1)
-		m_pHUD->animPlay(random_anim(mhud_shot1),TRUE,this,GetState());
+	if (m_magazine.size() == 2)
+		m_pHUD->animPlay(random_anim(mhud_shot2), TRUE, this, GetState());
+	else if(m_magazine.size()==1)
+		m_pHUD->animPlay(random_anim(mhud_shot1),TRUE, this, GetState());
 	else
-		m_pHUD->animPlay(random_anim(mhud.mhud_shots),TRUE,this,GetState());
+		m_pHUD->animPlay(random_anim(mhud.mhud_shots),TRUE, this, GetState());
 }
 
 void CWeaponBM16::PlayAnimReload()
@@ -50,6 +75,42 @@ void CWeaponBM16::PlayAnimReload()
 	else
 		m_pHUD->animPlay(random_anim(mhud.mhud_reload),TRUE,this,GetState());
 
+}
+
+void CWeaponBM16::PlayAnimShow()
+{
+	VERIFY(GetState() == eShowing);
+
+	switch (m_magazine.size())
+	{
+	case 0: {
+		m_pHUD->animPlay(random_anim(mhud.mhud_show), FALSE, this, GetState());
+	}break;
+	case 1: {
+		m_pHUD->animPlay(random_anim(mhud_show1), FALSE, this, GetState());
+	}break;
+	case 2: {
+		m_pHUD->animPlay(random_anim(mhud_show2), FALSE, this, GetState());
+	}break;
+	};
+}
+
+void CWeaponBM16::PlayAnimHide()
+{
+	VERIFY(GetState() == eHiding);
+
+	switch (m_magazine.size())
+	{
+	case 0: {
+		m_pHUD->animPlay(random_anim(mhud.mhud_hide), TRUE, this, GetState());
+	}break;
+	case 1: {
+		m_pHUD->animPlay(random_anim(mhud_hide1), TRUE, this, GetState());
+	}break;
+	case 2: {
+		m_pHUD->animPlay(random_anim(mhud_hide2), TRUE, this, GetState());
+	}break;
+	};
 }
 
 void CWeaponBM16::PlayAnimIdle( u8 state = eIdle ) {
