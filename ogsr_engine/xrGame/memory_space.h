7c3e92eaedff97feab85948a8725b15e11981d41
Denis Shaposhnikov <dsh@bamus.cz>
2018-09-30 11:08:31 +0200

    Что бы маска была 64 бита на любой архитектуре

--- ogsr_engine/xrGame/memory_space.h
+++ ogsr_engine/xrGame/memory_space.h
@@ -40,7 +40,7 @@ class CEntityAlive;
 class CGameObject;
 
 namespace MemorySpace {
-	using squad_mask_type = size_t;
+	using squad_mask_type = unsigned __int64;
 
 	struct CNotYetVisibleObject {
 		const CGameObject			*m_object;
