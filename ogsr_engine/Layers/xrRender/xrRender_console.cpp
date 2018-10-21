a1cce6d57743ae66d559cd8ead050d257f33681c
Denis Shaposhnikov <dsh@bamus.cz>
2018-10-21 11:48:57 +0200

    Увеличил верхнее значения для r2_sun_near_border с 1 до 3
    На широких мониторах его нужно устанавливать больше единицы, что бы
    избавиться от тени по краям экрана.

--- ogsr_engine/Layers/xrRender/xrRender_console.cpp
+++ ogsr_engine/Layers/xrRender/xrRender_console.cpp
@@ -554,7 +554,7 @@ void		xrRender_initconsole	()
 	CMD4(CCC_Float,		"r2_sun_tsm_proj",		&ps_r2_sun_tsm_projection,	.001f,	0.8f	);
 	CMD4(CCC_Float,		"r2_sun_tsm_bias",		&ps_r2_sun_tsm_bias,		-0.5,	+0.5	);
 	CMD4(CCC_Float,		"r2_sun_near",			&ps_r2_sun_near,			1.f,	/*50.f*/150.f	);	// KD: extended from 50 to 150
-	CMD4(CCC_Float,		"r2_sun_near_border",	&ps_r2_sun_near_border,		.5f,	1.0f	);
+	CMD4(CCC_Float,		"r2_sun_near_border",	&ps_r2_sun_near_border,		.5f,	3.0f	);
 	CMD4(CCC_Float,		"r2_sun_limit",			&ps_r2_sun_limit, 1.f, 180.f );
 	CMD4(CCC_Float,		"r2_sun_depth_far_scale",&ps_r2_sun_depth_far_scale,0.5,	1.5		);
 	CMD4(CCC_Float,		"r2_sun_depth_far_bias",&ps_r2_sun_depth_far_bias,	-0.5,	+0.5	);
