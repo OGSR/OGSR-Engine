3676dbe9665bed7512566930c82f10be947668d7
KRodin <stalker.moderator@gmail.com>
2018-11-05 00:24:46 +0300

    Переписал stacktrace_collector
    Использовал код из https://github.com/OpenXRay/xray-16

--- ogsr_engine/xrCore/stacktrace_collector.h
+++ ogsr_engine/xrCore/stacktrace_collector.h
@@ -1,19 +1,4 @@
 #pragma once
 
-static constexpr size_t MaxStackTraceDepth = 256u;
-static constexpr size_t MaxFrameLength = 4096u;
-
-struct StackTraceInfo {
-	char frames[MaxStackTraceDepth * (MaxFrameLength + 1)];
-	size_t count;
-
-	char* operator[] (const size_t index) {
-		return frames + (MaxFrameLength + 1) * index;
-	}
-};
-
-void BuildStackTrace(StackTraceInfo& stackTrace);
-
-inline void BuildStackTrace(EXCEPTION_POINTERS*, StackTraceInfo& stackTrace) {
-	BuildStackTrace(stackTrace);
-}
+std::stringstream BuildStackTrace(const char* header, u16 maxFramesCount = 512);
+std::stringstream BuildStackTrace(const char* header, PCONTEXT threadCtx, u16 maxFramesCount = 512);
