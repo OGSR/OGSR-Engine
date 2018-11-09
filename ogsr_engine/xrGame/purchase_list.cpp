f17ad2c7cca5a8d024db40c7b077a6fc5487dcd8
Sergii T <joye.ramone@gmail.com>
2018-11-09 23:32:46 +0200

    Added new engine callback: trade_purchase_item_process Called for every item when spawn supplies for traders. Signature trade_purchase_item_process(npc, sobj) npc - trader, sobj item server object.

--- ogsr_engine/xrGame/purchase_list.cpp
+++ ogsr_engine/xrGame/purchase_list.cpp
@@ -12,6 +12,7 @@
 #include "gameobject.h"
 #include "ai_object_location.h"
 #include "level.h"
+#include "xrServer_Object_Base.h"
 
 static float min_deficit_factor = .3f;
 
@@ -20,6 +21,17 @@ void CPurchaseList::process	(CInifile &ini_file, LPCSTR section, CInventoryOwner
 	owner.sell_useless_items();
 
 	m_deficits.clear		();
+
+	luabind::functor<void> lua_function;
+
+	if (pSettings->line_exist("engine_callbacks", "trade_purchase_item_process"))
+	{
+		const LPCSTR callback = pSettings->r_string("engine_callbacks", "trade_purchase_item_process");
+		if (!ai().script_engine().functor(callback, lua_function))
+		{
+			Msg("Cannot get engine callback %s!", pSettings->r_string("engine_callbacks", "trade_purchase_item_process"));
+		}
+	}
 
 	const CGameObject		&game_object = smart_cast<const CGameObject &>(owner);
 	CInifile::Sect			&S = ini_file.r_section(section);
@@ -32,12 +44,15 @@ void CPurchaseList::process	(CInifile &ini_file, LPCSTR section, CInventoryOwner
 			game_object,
 			I.first,
 			atoi(_GetItem(I.second.c_str(),0,temp0)),
-			(float)atof(_GetItem(I.second.c_str(),1,temp1))
+			(float)atof(_GetItem(I.second.c_str(),1,temp1)),
+			lua_function
 		);
 	}
 }
 
-void CPurchaseList::process	(const CGameObject &owner, const shared_str &name, const u32 &count, const float &probability)
+#include "script_game_object.h"
+
+void CPurchaseList::process	(const CGameObject &owner, const shared_str &name, const u32 &count, const float &probability, luabind::functor<void> lua_function)
 {
 	VERIFY3					(count,"Invalid count for section in the purchase list",*name);
 	VERIFY3					(!fis_zero(probability,EPS_S),"Invalid probability for section in the purchase list",*name);
@@ -51,7 +66,18 @@ void CPurchaseList::process	(const CGameObject &owner, const shared_str &name, c
 			continue;
 
 		++j;
-		Level().spawn_item		(*name,position,level_vertex_id,id,false);
+
+		CSE_Abstract* _abstract = Level().spawn_item(*name, position, level_vertex_id, id, true);
+
+		if (lua_function)
+		{
+			lua_function(owner.lua_game_object(), _abstract);
+		}
+
+		NET_Packet P;
+		_abstract->Spawn_Write(P, TRUE);
+		Level().Send(P, net_flags(TRUE));
+		F_entity_Destroy(_abstract);
 	}
 
 	VERIFY3(m_deficits.find(name) == m_deficits.end(),"Duplicate section in the purchase list",*name);
