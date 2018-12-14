d192e5f89569c6d060fa5e7dbb6fd33f9865576d
Denis Shaposhnikov <dsh@bamus.cz>
2018-12-14 18:36:56 +0100

    Просто поправил стиль для понятности

--- ogsr_engine/xrGame/ui/UIIconParams.h
+++ ogsr_engine/xrGame/ui/UIIconParams.h
@@ -1,35 +1,29 @@
-// icon_params.h
 #pragma once
 
-
-
-class CIconParams
-{
+class CIconParams {
 protected:
-	bool			loaded;
+  bool loaded;
+
 public:
-	int				icon_group;
-	float			grid_x;
-	float			grid_y;
-	float			grid_width;
-	float			grid_height;
-	bool			show_condition;
-	shared_str		name;
-	shared_str		section;
+  int        icon_group;
+  float      grid_x;
+  float      grid_y;
+  float      grid_width;
+  float      grid_height;
+  bool       show_condition;
+  shared_str name;
+  shared_str section;
 
-	CIconParams()  
-	{
-		loaded = false;
-		icon_group = 0;
-		grid_x = grid_y = 0.f;
-		grid_width = grid_height = 0.f;
-		show_condition = false;
-	}
-	CIconParams				(const shared_str section)  { Load(section); }
+  CIconParams() {
+    loaded     = false;
+    icon_group = 0;
+    grid_x     = grid_y = 0.f;
+    grid_width = grid_height = 0.f;
+    show_condition = false;
+  }
+  CIconParams( const shared_str section ) { Load( section ); }
 
-	void					Load(const shared_str item_section);
-
-	ref_shader&				get_shader()		const;
-	Frect&					original_rect()		const;  // used static variable!
-
+  void        Load( const shared_str item_section );
+  ref_shader& get_shader() const;
+  Frect&      original_rect() const;
 };
