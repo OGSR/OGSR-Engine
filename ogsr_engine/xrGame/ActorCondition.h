dd7c1d18fd80ec70a9b347fd64593cfb683d48af
Sergii T <joye.ramone@gmail.com>
2019-01-29 14:43:43 +0200

    Заменил m_bLimping, m_bCantWalk и m_bCantSprint флагами состояний что бы они сохранялись при save\load. Иначе, если сохраниться между limping_health_begin и limping_health_end (и соотвествунно другими парамами параметров) - после загрузки эффект пропадал

--- ogsr_engine/xrGame/ActorCondition.h
+++ ogsr_engine/xrGame/ActorCondition.h
@@ -16,7 +16,7 @@ class CActor;
 
 class CActorCondition: public CEntityCondition {
 	friend class CScriptActor;
-private:
+public:
 	typedef CEntityCondition inherited;
 	enum {	eCriticalPowerReached			=(1<<0),
 			eCriticalMaxPowerReached		=(1<<1),
@@ -26,6 +26,10 @@ private:
 			eWeaponJammedReached			=(1<<5),
 			ePhyHealthMinReached			=(1<<6),
 			eCantWalkWeight					=(1<<7),
+
+			eLimping = (1 << 8),
+			eCantWalk = (1 << 9),
+			eCantSprint = (1 << 10),
 			};
 	Flags16											m_condition_flags;
 private:
@@ -46,10 +50,10 @@ public:
 	virtual void 		ChangeSatiety				(float value);
 
 	// хромание при потере сил и здоровья
-	virtual	bool		IsLimping					() const;
-	virtual bool		IsCantWalk					() const;
+	virtual	bool		IsLimping					();
+	virtual bool		IsCantWalk					();
 	virtual bool		IsCantWalkWeight			();
-	virtual bool		IsCantSprint				() const;
+	virtual bool		IsCantSprint				();
 
 	virtual void		UpdatePower();
 
@@ -96,11 +100,6 @@ protected:
 	float m_fAccelK;
 	float m_fSprintK;
 	
-
-	mutable bool m_bLimping;
-	mutable bool m_bCantWalk;
-	mutable bool m_bCantSprint;
-
 	//порог силы и здоровья меньше которого актер начинает хромать
 	float m_fLimpingPowerBegin;
 	float m_fLimpingPowerEnd;
