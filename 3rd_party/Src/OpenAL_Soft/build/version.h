d8c2377e0e5c991fc452cea72bfda5769569a06e
KRodin <stalker.moderator@gmail.com>
2019-04-01 12:05:18 +0300

    Выкинул исходники OpenAL Soft из солюшена

--- 3rd_party/Src/OpenAL_Soft/build/version.h
+++
@@ -1,8 +0,0 @@
-/* Define to the library version */
-#define ALSOFT_VERSION "1.19.0"
-
-/* Define the branch being built */
-#define ALSOFT_GIT_BRANCH "UNKNOWN"
-
-/* Define the hash of the head commit */
-#define ALSOFT_GIT_COMMIT_HASH "unknown"
