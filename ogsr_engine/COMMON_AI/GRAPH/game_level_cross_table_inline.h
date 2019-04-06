a654bc1772cc088b48b5d15bc3c7758bb09f677e
Sergii T <joye.ramone@gmail.com>
2019-04-06 23:51:16 +0300

    Removed (inlined) AI_COMPILER, PRIQUEL and USE_DESIGNER_KEY defines

--- ogsr_engine/COMMON_AI/GRAPH/game_level_cross_table_inline.h
+++ ogsr_engine/COMMON_AI/GRAPH/game_level_cross_table_inline.h
@@ -8,17 +8,10 @@
 
 #pragma once
 
-#ifndef PRIQUEL
-#ifdef AI_COMPILER
-IC CGameLevelCrossTable::CGameLevelCrossTable(LPCSTR fName)
-#else // AI_COMPILER
-		IC CGameLevelCrossTable::CGameLevelCrossTable	()
-#endif // AI_COMPILER
+IC CGameLevelCrossTable::CGameLevelCrossTable	()
 {
-#ifndef AI_COMPILER
 	string_path			fName;
 	FS.update_path		(fName,"$level$",CROSS_TABLE_NAME);
-#endif // AI_COMPILER
 
 	m_tpCrossTableVFS	= FS.r_open(fName);
 	R_ASSERT2			(m_tpCrossTableVFS,"Can't open cross table!");
@@ -34,27 +27,14 @@ IC CGameLevelCrossTable::CGameLevelCrossTable(LPCSTR fName)
 	R_ASSERT2			(m_chunk,"Cross table is corrupted!");
 	m_tpaCrossTable		= (CCell*)m_chunk->pointer();
 };
-#else // PRIQUEL
-IC CGameLevelCrossTable::CGameLevelCrossTable	(const void *buffer, const u32 &buffer_size)
-{
-	Memory.mem_copy		(&m_tCrossTableHeader,buffer,sizeof(m_tCrossTableHeader));
-	buffer				= (const u8*)buffer + sizeof(m_tCrossTableHeader);
-
-	R_ASSERT2			(m_tCrossTableHeader.version() == XRAI_CURRENT_VERSION,"Cross table version mismatch!");
-	
-	m_tpaCrossTable		= (CCell*)buffer;
-}
-#endif // PRIQUEL
 
 IC CGameLevelCrossTable::~CGameLevelCrossTable	()
 {
-#ifndef PRIQUEL
 	VERIFY				(m_chunk);
 	m_chunk->close		();
 	
 	VERIFY				(m_tpCrossTableVFS);
 	FS.r_close			(m_tpCrossTableVFS);
-#endif // PRIQUEL
 };
 
 IC const CGameLevelCrossTable::CCell &CGameLevelCrossTable::vertex(u32 level_vertex_id) const
