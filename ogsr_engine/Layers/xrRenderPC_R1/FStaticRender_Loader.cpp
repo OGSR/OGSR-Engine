173e3c935c64aee2dd8fa7b85ecd3916e8e5402a
KRodin <stalker.moderator@gmail.com>
2019-04-12 16:41:21 +0300

    Ещё потерянные правки для x64

--- ogsr_engine/Layers/xrRenderPC_R1/FStaticRender_Loader.cpp
+++ ogsr_engine/Layers/xrRenderPC_R1/FStaticRender_Loader.cpp
@@ -335,7 +335,7 @@ void CRender::LoadSectors(IReader* fs)
 
 		// build portal model
 		rmPortals = xr_new	<CDB::MODEL> ();
-		rmPortals->build	(CL.getV(),int(CL.getVS()),CL.getT(),int(CL.getTS()));
+		rmPortals->build(CL.getV(),int(CL.getVS()),CL.getT(),int(CL.getTS()), nullptr, nullptr, false);
 	} else {
 		rmPortals = 0;
 	}
