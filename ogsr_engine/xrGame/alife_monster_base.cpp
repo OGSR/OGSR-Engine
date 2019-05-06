e72f580f8b916f5cd5575b415a7718966cd1b25e
Denis Shaposhnikov <dsh@bamus.cz>
2019-05-07 00:28:42 +0200

    Расширил возможности Spawn_Inventory_Item_Section
    Теперь в этом параметре может быть список через запятую вида
    
    Spawn_Inventory_Item_Section = mutant_krovosos_red_jaw, 0.05, mutant_krovosos_jaw, 0.2
    
    т.е. если рандом меньше 0.05, то у мутанта будет
    mutant_krovosos_red_jaw. Если нет, то генерируется новое случайное
    число и проверяется следующая часть и так далее, пока не кончится
    список или не будет что-то заспаунено. Если в этом списке только один
    элемент, то все работает по старому и его вероятность берется из
    Spawn_Inventory_Item_Probability.
    
    Spawn_Inventory_Item_Probability теперь необязательный параметр и его
    значение по умолчанию - 0. Это что бы можно было его не указывать,
    если в Spawn_Inventory_Item_Section находится список с вероятностями.

--- ogsr_engine/xrGame/alife_monster_base.cpp
+++ ogsr_engine/xrGame/alife_monster_base.cpp
@@ -20,12 +20,24 @@ void CSE_ALifeMonsterBase::on_spawn				()
 		return;
 
 	LPCSTR						item_section = pSettings->r_string(s_name,"Spawn_Inventory_Item_Section");
-	float						spawn_probability = pSettings->r_float(s_name,"Spawn_Inventory_Item_Probability");
-	float						probability = randF();
-	if ((probability >= spawn_probability) && !fsimilar(spawn_probability,1.f))
-		return;
-
-	alife().spawn_item(item_section,o_Position,m_tNodeID,m_tGraphID,ID)->ID_Parent = ID;
+	float spawn_probability = READ_IF_EXISTS( pSettings, r_float, s_name, "Spawn_Inventory_Item_Probability", 0.f );
+        if ( item_section && item_section[ 0 ] ) {
+          string128 item;
+          int count = _GetItemCount( item_section );
+          for ( int i = 0; i < count; i += 2 ) {
+            _GetItem( item_section, i, item );
+            float spawn_prob = spawn_probability;
+            if ( i + 1 < count ) {
+              string128 tmp;
+              spawn_prob = atof( _GetItem( item_section, i + 1, tmp ) );
+            }
+            float probability = randF();
+            if ( probability < spawn_prob || fsimilar( spawn_prob, 1.f ) ) {
+              alife().spawn_item( item, o_Position, m_tNodeID, m_tGraphID, ID )->ID_Parent = ID;
+              break;
+            }
+          }
+        }
 }
 
 extern void add_online_impl		(CSE_ALifeDynamicObject *object, const bool &update_registries);
