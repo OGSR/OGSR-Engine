5b04eef9a4d2426e4eb47c606d552ca375f30086
Denis Shaposhnikov <dsh@bamus.cz>
2019-05-27 11:48:47 +0200

    Починил картинки в статьях энциклопедии
    Без этого их не было. При переходе на рендеры ЗП эта строчка исчезла.

--- ogsr_engine/xrGame/ui/UIStatic.cpp
+++ ogsr_engine/xrGame/ui/UIStatic.cpp
@@ -133,6 +133,7 @@ void CUIStatic::InitTextureEx(LPCSTR tex_name, LPCSTR sh_name)
 
 	Fvector2 p						= GetWndPos();
 	m_UIStaticItem.SetPos			(p.x, p.y);
+	m_bAvailableTexture = true;
 }
 
 void  CUIStatic::Draw()
