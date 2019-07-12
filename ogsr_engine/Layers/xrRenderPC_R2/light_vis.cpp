536ad669a0c55ab56f5d873eb444cbfd01dea900
Denis Shaposhnikov <dsh@bamus.cz>
2019-07-12 17:08:02 +0200

    Костыль против мерцающих теней от точечных источников
    По идее, этот код определяется видимость сферы света и либо рендерит
    этот свет, либо нет. А по сути получается так, что то рендерит, то
    нет. Здесь уже был коэффициент, применяемый к радиусу света, что бы
    свет всенда рендерился, если камера находится внутри этого
    радиуса. Для точечных источников я этот коэффициент просто увеличил в
    10 раз, что бы если мы не далеко от источника света, то пусть
    рендерится всегда, без проверок. Это убрало мерцание теней на худе и
    мерцающий квадрат на текстуре под лампочкой.

--- ogsr_engine/Layers/xrRenderPC_R2/light_vis.cpp
+++ ogsr_engine/Layers/xrRenderPC_R2/light_vis.cpp
@@ -36,7 +36,8 @@ void	light::vis_prepare			()
 	if (ps_r2_ls_flags.test(R2FLAG_EXP_DONT_TEST_UNSHADOWED) && !flags.bShadow)	skiptest=true;
 	if (ps_r2_ls_flags.test(R2FLAG_EXP_DONT_TEST_SHADOWED) && flags.bShadow)	skiptest=true;
 
-	if (skiptest || Device.vCameraPosition.distance_to(spatial.sphere.P)<=(spatial.sphere.R*1.01f+safe_area))	{	// small error
+	float k = ( flags.type == IRender_Light::OMNIPART && flags.bShadow ) ? 10.01f : 1.01f;
+	if (skiptest || Device.vCameraPosition.distance_to(spatial.sphere.P)<=(spatial.sphere.R*k+safe_area))	{	// small error
 		vis.visible		=	true;
 		vis.pending		=	false;
 		vis.frame2test	=	frame	+ ::Random.randI(delay_small_min,delay_small_max);
