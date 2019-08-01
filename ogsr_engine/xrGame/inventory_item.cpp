8425d145a333433a7f8b053d238cbfec3e3c8e68
Denis Shaposhnikov <dsh@bamus.cz>
2019-08-01 18:12:35 +0200

    Добавил show_brief_info в секцию оружия
    По умолчанию имеет значение true. Если указать
    
    show_brief_info = false
    
    то при активации этого предмета на хуже не будет отображаться
    информация об оружии.

--- ogsr_engine/xrGame/inventory_item.cpp
+++ ogsr_engine/xrGame/inventory_item.cpp
@@ -197,6 +197,8 @@ void CInventoryItem::Load(LPCSTR section)
 	m_fPsyHealthRestoreSpeed = READ_IF_EXISTS( pSettings, r_float, section,	"psy_health_restore_speed", 0.f );
 	m_fRadiationRestoreSpeed = READ_IF_EXISTS( pSettings, r_float, section,	"radiation_restore_speed", 0.f );
 	m_always_ungroupable = READ_IF_EXISTS( pSettings, r_bool, section, "always_ungroupable", false );
+
+	m_need_brief_info = READ_IF_EXISTS( pSettings, r_bool, section, "show_brief_info", true );
 }
 
 
