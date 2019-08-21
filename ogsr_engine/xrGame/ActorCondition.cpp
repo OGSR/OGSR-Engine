73dd442f2137d2032e95f498a52cb683b7d304be
KRodin <stalker.moderator@gmail.com>
2019-08-21 10:54:31 +0300

    Восстановил https://github.com/OGSR/OGSR-Engine/commit/80960ddc3e56bd0a33a5d582cbd98a2b1aa239d7
    Фикс затёрся при каком-то мерже полгода назад

--- ogsr_engine/xrGame/ActorCondition.cpp
+++ ogsr_engine/xrGame/ActorCondition.cpp
@@ -626,7 +626,7 @@ void CActorCondition::UpdateTutorialThresholds()
 		strcpy_s(cb_name, "_G.on_actor_psy");
 	}
 
-	if (b && !m_condition_flags.test(eCantWalkWeight)) {
+	if (b && m_condition_flags.test(eCantWalkWeight)) {
 		b = false;
 		strcpy_s(cb_name, "_G.on_actor_cant_walk_weight");
 	}
