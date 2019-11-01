d19cdbf0544f8e8eefb56e0c42d4718c63ee9937
Denis Shaposhnikov <dsh@bamus.cz>
2019-11-01 17:19:07 +0100

    Добавить раненного пойдет только тот, кому он назначен, а не всей гурьбой.
    А то была какая-то феерическая чухня. Если у сталкера есть враг и он
    ранен, значит можно идти его добивать. Притом, что возможна и
    постоянно встречаются ситуации, когда враг один, а сталкеров много. И
    вот они всей толпой идит к нему. Но! Стреляет в него только один,
    которому он назначен. Вот из CStalkerActionPrepareWounded::execute
    
    	if (object().agent_manager().enemy().wounded_processor(enemy) != object().ID())
    		return;
    
    И какого черта, тогда идут все остальные? Для моральной поддержки?

--- ogsr_engine/xrGame/stalker_danger_property_evaluators.cpp
+++ ogsr_engine/xrGame/stalker_danger_property_evaluators.cpp
@@ -22,6 +22,7 @@
 #include "cover_point.h"
 #include "stalker_movement_restriction.h"
 #include "enemy_manager.h"
+#include "agent_enemy_manager.h"
 
 using namespace StalkerDecisionSpace;
 
@@ -253,6 +254,10 @@ _value_type CStalkerPropertyEvaluatorEnemyWounded::evaluate	()
 	const CAI_Stalker			*stalker = smart_cast<const CAI_Stalker *>(enemy);
 	if (!stalker)
 		return					(false);
+
+	ALife::_OBJECT_ID processor_id = object().agent_manager().enemy().wounded_processor( enemy );
+	if ( processor_id != object().ID() )
+	  return false;
 
 	return						(stalker->wounded(&object().movement().restrictions()));
 }
