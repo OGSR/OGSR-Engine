1586d6713c2dcb990b8d3341b237999765c81f8a
Denis Shaposhnikov <dsh@bamus.cz>
2019-11-14 16:32:23 +0100

    Переделал костыль прости мерцающих теней на худе?
    который делал вот тут
    https://github.com/dsh2dsh/OGSR-Engine/commit/f500405d0ed158f3fccfda5053ca16ebd684216e
    
    Тот костыль не во всех случаях помогал. 65.f было подобрано опытным
    путем в подвале Сидоровича. При 60.f тени все еще мерцали, при 65.f -
    прекратили мерцать.
    
    Вообще, не понятно, а точно ли нужна вся вот эта вот фигня, с
    проверками видимости сфер. А то может это очередная "оптимизация" о
    GSC, которая на самом деле ничего не оптимизирует? С помощью
    
    r2_exp_donttest_shad on
    
    эту проверку можно полностью выключить и тогда тени перестанут мерцать
    без всяких костылей. Изменения ФПС, при этом, я не вижу.

--- ogsr_engine/Layers/xrRenderPC_R2/light_vis.cpp
+++ ogsr_engine/Layers/xrRenderPC_R2/light_vis.cpp
@@ -27,7 +27,7 @@ void	light::vis_prepare			()
 		float	x0	= VIEWPORT_NEAR/_cos	(a0);
 		float	x1	= VIEWPORT_NEAR/_cos	(a1);
 		float	c	= _sqrt					(x0*x0 + x1*x1);
-		safe_area	= _max(_max(VIEWPORT_NEAR,_max(x0,x1)),c);
+		safe_area	= _max(_max(VIEWPORT_NEAR,_max(x0,x1)),c) + 65.f;
 	}
 
 	//Msg	("sc[%f,%f,%f]/c[%f,%f,%f] - sr[%f]/r[%f]",VPUSH(spatial.center),VPUSH(position),spatial.radius,range);
@@ -36,8 +36,7 @@ void	light::vis_prepare			()
 	if (ps_r2_ls_flags.test(R2FLAG_EXP_DONT_TEST_UNSHADOWED) && !flags.bShadow)	skiptest=true;
 	if (ps_r2_ls_flags.test(R2FLAG_EXP_DONT_TEST_SHADOWED) && flags.bShadow)	skiptest=true;
 
-	float k = ( flags.type == IRender_Light::OMNIPART && flags.bShadow ) ? 10.01f : 1.01f;
-	if (skiptest || Device.vCameraPosition.distance_to(spatial.sphere.P)<=(spatial.sphere.R*k+safe_area))	{	// small error
+	if (skiptest || Device.vCameraPosition.distance_to(spatial.sphere.P)<=(spatial.sphere.R*1.01f+safe_area))	{	// small error
 		vis.visible		=	true;
 		vis.pending		=	false;
 		vis.frame2test	=	frame	+ ::Random.randI(delay_small_min,delay_small_max);
