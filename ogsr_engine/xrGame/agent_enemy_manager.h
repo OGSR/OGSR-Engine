b9f71d5162e96fbb3d0dcca9b0911ba1d98e001c
Denis Shaposhnikov <dsh@bamus.cz>
2019-12-12 16:29:02 +0100

    Немного переделал процесс назначения раненных для добивания
    Назначение раненных происходит внутри группы. Группа - это, в
    простейшем случае, сталкеры одной группировки. Эти группы работают
    независимо друг от друга. Т.е если у нас один раненный и несколько
    сталкеров разных группировок, то этот раненный будет назначен
    представителю от каждой группировки. Т.е. его пойдет добивать по
    одному представителью от каждой группировки. Комитет эдакий.
    
    Тут я добавил глобальный вектор m_wounded_processors, через который мы
    будем отслеживать глобально, кому назначен раненный, а не в пределах
    группы, т.ч. добивать его пойдет только один сталкер, который будет
    ближе всех.

--- ogsr_engine/xrGame/agent_enemy_manager.h
+++ ogsr_engine/xrGame/agent_enemy_manager.h
@@ -27,6 +27,7 @@ private:
 	CAgentManager			*m_object;
 	ENEMIES					m_enemies;
 	WOUNDED_ENEMIES			m_wounded;
+	static WOUNDED_ENEMIES m_wounded_processors;
 	bool					m_only_wounded_left;
 	bool					m_is_any_wounded;
 
@@ -60,6 +61,7 @@ public:
 			bool			wounded_processed	(const CEntityAlive *object) const;
 			bool			assigned_wounded	(const CEntityAlive *wounded, const CAI_Stalker *member);
 			bool			useful_enemy		(const CEntityAlive *enemy, const CAI_Stalker *member) const;
+			void remove_wounded_processor( const CEntityAlive* object );
 };
 
 #include "agent_enemy_manager_inline.h"
