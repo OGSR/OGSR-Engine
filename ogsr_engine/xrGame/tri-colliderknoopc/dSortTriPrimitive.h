29c7d94af7dcdc0e04af936e0899c988c5b682c9
Denis Shaposhnikov <dsh@bamus.cz>
2019-12-23 16:33:56 +0100

    Revert "Исправление проваливания предметов от @KRodinn"
    This reverts commit 5e81f215f7362520213e44f0b8be8ef1f38bf225.
    
    Получил странное зависание
    https://youtu.be/Y_DCA2--bHc?t=11371

--- ogsr_engine/xrGame/tri-colliderknoopc/dSortTriPrimitive.h
+++ ogsr_engine/xrGame/tri-colliderknoopc/dSortTriPrimitive.h
@@ -10,9 +10,6 @@
 #include "../PHDebug.h"
 #endif
 
-#pragma float_control(push)
-#pragma float_control(precise, on)
-
 inline bool negative_tri_set_ignored_by_positive_tri(const Triangle &neg_tri, const Triangle &pos_tri, const Fvector*	 V_array)
 {
 	bool common0 = neg_tri.T->verts[0] == pos_tri.T->verts[0] || neg_tri.T->verts[0] == pos_tri.T->verts[1] ||
@@ -388,6 +385,4 @@ inline int dcTriListCollider::dSortTriPrimitiveCollide(
 
 	return ret;
 }
-
-#pragma float_control(pop)
 #endif
