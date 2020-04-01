236104b5a99ab6e198ead1f38d84d86d31ef16a6
Krodin <16670637+KRodinn@users.noreply.github.com>
2020-04-01 17:18:55 +0300

    Update Actor.cpp

--- ogsr_engine/xrGame/Actor.cpp
+++ ogsr_engine/xrGame/Actor.cpp
@@ -904,7 +904,12 @@ void CActor::UpdateCL	()
 	}
 }
 
-#define TASKS_UPDATE_TIME 500u
+#if defined(OGSR_MOD) || defined(DSH_MOD)
+constexpr u32 TASKS_UPDATE_TIME = 500u;
+#else
+constexpr u32 TASKS_UPDATE_TIME = 1u;
+#endif
+
 float	NET_Jump = 0;
 void CActor::shedule_Update	(u32 DT)
 {
