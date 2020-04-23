946305f259a3482ad541e4de2e9d98cb76dfde1d
Krodin <16670637+KRodinn@users.noreply.github.com>
2020-04-23 10:10:51 +0300

    Добавил недостающие шейдеры

--- ogsr_engine/Layers/xrRenderDX10/dx10ResourceManager_Resources.cpp
+++ ogsr_engine/Layers/xrRenderDX10/dx10ResourceManager_Resources.cpp
@@ -167,17 +167,9 @@ SVS*	CResourceManager::_CreateVS		(LPCSTR _name)
 		//		LPCSTR						target		= NULL;
 
 		// duplicate and zero-terminate
-		IReader* file			= FS.r_open(cname);
-		//	TODO: DX10: HACK: Implement all shaders. Remove this for PS
-		if (!file)
-		{
-			string1024			tmp;
-			xr_sprintf			(tmp, "DX10: %s is missing. Replace with stub_default.vs", cname);
-			Msg					(tmp);
-			strconcat			(sizeof(cname), cname,::Render->getShaderPath(),"stub_default",".vs");
-			FS.update_path		(cname,	"$game_shaders$", cname);
-			file				= FS.r_open(cname);
-		}
+		IReader* file = FS.r_open(cname);
+		R_ASSERT2(file, cname);
+
 		u32	const size			= file->length();
 		char* const data		= (LPSTR)_alloca(size + 1);
 		CopyMemory				( data, file->pointer(), size );
@@ -270,20 +262,8 @@ SPS*	CResourceManager::_CreatePS			(LPCSTR _name)
 
 		// duplicate and zero-terminate
 		IReader* file = FS.r_open(cname);
-		//	TODO: DX10: HACK: Implement all shaders. Remove this for PS
-		if (!file)
-		{
-			string1024			tmp;
-			//	TODO: HACK: Test failure
-			//Memory.mem_compact();
-			xr_sprintf				(tmp, "DX10: %s is missing. Replace with stub_default.ps", cname);
-			Msg					(tmp);
-			strconcat					(sizeof(cname), cname,::Render->getShaderPath(),"stub_default",".ps");
-			FS.update_path				(cname,	"$game_shaders$", cname);
-			file = FS.r_open(cname);
-		}
+		R_ASSERT2(file, cname);
 
-		R_ASSERT2				( file, cname );
 		u32	const size			= file->length();
 		char* const data		= (LPSTR)_alloca(size + 1);
 		CopyMemory				( data, file->pointer(), size );
@@ -346,21 +326,8 @@ SGS*	CResourceManager::_CreateGS			(LPCSTR name)
 		FS.update_path				(cname,	"$game_shaders$", cname);
 
 		// duplicate and zero-terminate
-		IReader*		R		= FS.r_open(cname);
-		//	TODO: DX10: HACK: Implement all shaders. Remove this for PS
-		if (!R)
-		{
-			string1024			tmp;
-			//	TODO: HACK: Test failure
-			//Memory.mem_compact();
-			xr_sprintf				(tmp, "DX10: %s is missing. Replace with stub_default.gs", cname);
-			Msg					(tmp);
-			strconcat					(sizeof(cname), cname,::Render->getShaderPath(),"stub_default",".gs");
-			FS.update_path				(cname,	"$game_shaders$", cname);
-			R		= FS.r_open(cname);
-		}
-		IReader* file			= FS.r_open(cname);
-		R_ASSERT2				( file, cname );
+		IReader* file = FS.r_open(cname);
+		R_ASSERT2(file, cname);
 
 		// Select target
 		LPCSTR						c_target	= "gs_4_0";
