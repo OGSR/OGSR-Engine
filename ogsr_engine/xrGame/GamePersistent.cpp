b7de68b56fdf3487cf96d6b243220abaac806105
Krodin <16670637+KRodinn@users.noreply.github.com>
2020-06-17 14:45:39 +0300

    The hemi-light factor has been adjusted, below which the player is considered indoors (for weather effects)
    Thx @ShokerStlk

--- ogsr_engine/xrGame/GamePersistent.cpp
+++ ogsr_engine/xrGame/GamePersistent.cpp
@@ -214,7 +214,7 @@ void CGamePersistent::WeathersUpdate()
 	{
 		CActor* actor = smart_cast<CActor*>(Level().CurrentViewEntity());
 		BOOL bIndoor = TRUE;
-		if (actor) bIndoor = actor->renderable_ROS()->get_luminocity_hemi() < 0.05f;
+		if (actor) bIndoor = actor->renderable_ROS()->get_luminocity_hemi() < 0.04f; //--#SM+#-- [0.05f слишком завышен был]
 
 		int data_set = (Random.randF() < (1.f - Environment().CurrentEnv->weight)) ? 0 : 1;
 
@@ -386,7 +386,7 @@ void CGamePersistent::WeathersUpdate()
 	{
 		CActor* actor = smart_cast<CActor*>(Level().CurrentViewEntity());
 		BOOL bIndoor = TRUE;
-		if (actor) bIndoor = actor->renderable_ROS()->get_luminocity_hemi() < 0.05f;
+		if (actor) bIndoor = actor->renderable_ROS()->get_luminocity_hemi() < 0.04f; //--#SM+#-- [0.05f слишком завышен был]
 
 		int data_set = (Random.randF() < (1.f - Environment().CurrentEnv->weight)) ? 0 : 1;
 
