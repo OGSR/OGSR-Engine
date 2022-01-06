58e5322a78ef62b4d1bbc7530a9f20d29596cf08
xrKrodin <16670637+KRodinn@users.noreply.github.com>
2022-01-06 19:40:39 +0300

    Забыл убрать лог

--- ogsr_engine/xrGame/HudItem.cpp
+++ ogsr_engine/xrGame/HudItem.cpp
@@ -275,7 +275,7 @@ u32 CHudItem::PlayHUDMotion(std::initializer_list<const char*> Ms, const bool bM
 		dbg_anim_name += M;
 		dbg_anim_name += ", ";
 	}
-	Msg("~~[%s] Motions [%s] not found for [%s]", __FUNCTION__, dbg_anim_name.c_str(), HudSection().c_str());
+	//Msg("~~[%s] Motions [%s] not found for [%s]", __FUNCTION__, dbg_anim_name.c_str(), HudSection().c_str());
 
 	return 0;
 }
