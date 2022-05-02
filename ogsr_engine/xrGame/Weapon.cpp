589689c60ec28acf93e625a1f8d4392e3c25921f
xrKrodin <16670637+xrKrodin@users.noreply.github.com>
2022-05-02 23:56:12 +0300

    Подпёр костылём проблему с ode

--- ogsr_engine/xrGame/Weapon.cpp
+++ ogsr_engine/xrGame/Weapon.cpp
@@ -1737,7 +1737,10 @@ void CWeapon::reload			(LPCSTR section)
 
 void CWeapon::create_physic_shell()
 {
-	CPhysicsShellHolder::create_physic_shell();
+	//CPhysicsShellHolder::create_physic_shell();
+	//xrKrodin: Временный? "фикс" для оружия из ганслингера, валяющегося на земле. По непонятным причинам (много костей или хз от чего ещё) в некоторых случаях при рассчетах физики происходят краши в ode которые исправить невозможно.
+	m_pPhysicsShell = P_build_SimpleShell(this, 0.3f, false);
+	m_pPhysicsShell->SetMaterial(smart_cast<IKinematics*>(Visual())->LL_GetData(smart_cast<IKinematics*>(Visual())->LL_GetBoneRoot()).game_mtl_idx);
 }
 
 void CWeapon::activate_physic_shell()
