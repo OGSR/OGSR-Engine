c8d930b5022174b31ba883f363fccbaae772bd76
xrKrodin <16670637+xrKrodin@users.noreply.github.com>
2022-05-21 16:52:54 +0300

    Update ShaderResourceTraits.h

--- ogsr_engine/Layers/xrRender/ShaderResourceTraits.h
+++ ogsr_engine/Layers/xrRender/ShaderResourceTraits.h
@@ -73,7 +73,7 @@
     template<typename T>
 	inline T* CResourceManager::CreateShader(const char* name)
 	{
-		auto& sh_map = GetShaderMap<ShaderTypeTraits<T>::MapType>();
+		auto& sh_map = GetShaderMap<typename ShaderTypeTraits<T>::MapType>();
 		auto I = sh_map.find(name);
 
 		if (I!=sh_map.end())
@@ -128,13 +128,13 @@
 	template<typename T>
 	inline void CResourceManager::DestroyShader(const T* sh)
 	{
-		auto& sh_map = GetShaderMap<ShaderTypeTraits<T>::MapType>();
+		auto& sh_map = GetShaderMap<typename ShaderTypeTraits<T>::MapType>();
 
 		if (0==(sh->dwFlags&xr_resource_flagged::RF_REGISTERED))
 			return;
 
 		LPSTR N = LPSTR(*sh->cName);
-		typename ShaderTypeTraits<T>::MapType::iterator I = sh_map.find(N);
+		auto I = sh_map.find(N);
 		
 		if (I!=sh_map.end())
 		{
