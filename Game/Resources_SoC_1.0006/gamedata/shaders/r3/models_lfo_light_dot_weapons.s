c72e083e0c9c0ea5ed98951a7e542941f7ef82a1
xrKrodin <16670637+xrKrodin@users.noreply.github.com>
2022-06-26 22:41:06 +0300

    Апдейт шейдеров из OGSR: GA

---
+++ Game/Resources_SoC_1.0006/gamedata/shaders/r3/models_lfo_light_dot_weapons.s
@@ -0,0 +1,10 @@
+function normal   (shader, t_base, t_second, t_detail)
+	shader:begin  ("model_def_lplanes","models_lfo_light_dot_weapons")
+      : fog       	(true)
+      : zb        	(true,false)
+      : blend     	(true,blend.srcalpha,blend.invsrcalpha)
+      : aref      	(true,0)
+      : sorting	  	(2,true)
+      : distort   	(true)
+	shader:dx10texture	("s_base",	t_base)
+end
