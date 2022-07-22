daccf093c7184c5e3124f35699eb9e15b58818d7
xrSimpodin <16670637+xrSimpodin@users.noreply.github.com>
2022-07-22 13:45:12 +0300

    revert 6d2934961a036c5ab0ee19ef75c6c6ae3c565271

--- ogsr_engine/Layers/xrRenderPC_R4/r4.cpp
+++ ogsr_engine/Layers/xrRenderPC_R4/r4.cpp
@@ -304,8 +304,8 @@ void					CRender::create					()
 	o.dx10_msaa_samples = (1 << ps_r3_msaa);
 
 	o.dx10_msaa_opt		= ps_r2_ls_flags.test(R3FLAG_MSAA_OPT);
-	o.dx10_msaa_opt		= o.dx10_msaa_opt && o.dx10_msaa && ( HW.FeatureLevel >= D3D_FEATURE_LEVEL_10_1 );
-	//		|| o.dx10_msaa && (HW.FeatureLevel >= D3D_FEATURE_LEVEL_11_0);
+	o.dx10_msaa_opt		= o.dx10_msaa_opt && o.dx10_msaa && ( HW.FeatureLevel >= D3D_FEATURE_LEVEL_10_1 )
+			|| o.dx10_msaa && (HW.FeatureLevel >= D3D_FEATURE_LEVEL_11_0);
 
 	//o.dx10_msaa_hybrid	= ps_r2_ls_flags.test(R3FLAG_MSAA_HYBRID);
 	o.dx10_msaa_hybrid	= ps_r2_ls_flags.test((u32)R3FLAG_USE_DX10_1);
@@ -1175,6 +1175,30 @@ HRESULT	CRender::shader_compile			(
 		}
 	}
 
+    if( o.dx10_msaa )
+	{
+		static char def[ 256 ];
+		//if( m_MSAASample < 0 )
+		//{
+			def[0]= '0';
+		//	sh_name[len]='0'; ++len;
+		//}
+		//else
+		//{
+		//	def[0]= '0' + char(m_MSAASample);
+		//	sh_name[len]='0' + char(m_MSAASample); ++len;
+		//}
+		def[1] = 0;
+		defines[def_it].Name		=	"ISAMPLE";
+		defines[def_it].Definition	=	def;
+		def_it						++	;
+		sh_name[len]='0'; ++len;
+	}
+	else
+	{
+		sh_name[len]='0'; ++len;
+	}
+
 	// skinning
 	if (m_skinning<0)		{
 		defines[def_it].Name		=	"SKIN_NONE";
@@ -1377,17 +1401,6 @@ HRESULT	CRender::shader_compile			(
 	   defines[def_it].Definition	= samples;	
 	   def_it						++;
 	   sh_name[len]='0'+char(o.dx10_msaa_samples); ++len;
-
-	   static char def[256];
-	   if (m_MSAASample < 0 || o.dx10_msaa_opt)
-		   def[0] = '0';
-	   else
-		   def[0] = '0' + char(m_MSAASample);
-
-	   def[1] = 0;
-	   defines[def_it].Name = "ISAMPLE";
-	   defines[def_it].Definition = def;
-	   def_it++;
 
 	   if( o.dx10_msaa_opt )
 	   {
