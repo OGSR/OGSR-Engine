7869d1f8126ebe378bc65ccf49fa90109b142ef3
xrSimpodin <16670637+xrSimpodin@users.noreply.github.com>
2022-07-31 16:25:55 +0300

    Убрал шейдеры для R2

--- Game/Resources_SoC_1.0006/gamedata/shaders/r2/models_lfo_light_dot_weapons.s
+++
@@ -1,9 +0,0 @@
-function normal		(shader, t_base, t_second, t_detail)
-  shader:begin  	("models_lfo_light_dot_weapons","models_lfo_light_dot_weapons")
-      : fog    		(false)
-      : zb     		(true,false)
-      : blend   	(true,blend.srcalpha,blend.one)
-      : aref    	(true,0)
-      : sorting		(2, true)
-  shader:sampler	("s_base")      :texture  (t_base)    : clamp()
-end
--- Game/Resources_SoC_1.0006/gamedata/shaders/r2/ogsr_rain_drops.s
+++
@@ -1,6 +0,0 @@
-function element_0(shader, t_base, t_second, t_detail)
-	shader:begin("null", "ogsr_rain_drops")
-		:fog(false)
-		:zb(false, false)
-	shader:sampler("s_image"):texture("$user$generic0"):clamp():f_none()
-end
