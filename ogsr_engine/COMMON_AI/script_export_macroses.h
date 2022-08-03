bc23d08fd569a12193caa77f06325b3f1edb7261
xrSimpodin <16670637+xrSimpodin@users.noreply.github.com>
2022-08-03 13:52:15 +0300

    Тут тоже убрал игнор ошибок

--- ogsr_engine/COMMON_AI/script_export_macroses.h
+++ ogsr_engine/COMMON_AI/script_export_macroses.h
@@ -29,49 +29,17 @@
 #define DEFINE_LUABIND_VIRTUAL_FUNCTION_EXPLICIT_CONST_0(a,b,c,d) \
 	.def(#c, (d (a::*)() const)(&a::c), (d (*)(const a*))(&b::c##_static))
 
-#ifndef LUABIND_NO_EXCEPTIONS
-#	define CAST_FAILED(v_func_name,ret_type) \
-		catch(luabind::cast_failed exception) {										\
-			ai().script_engine().script_log (ScriptStorage::eLuaMessageTypeError,"SCRIPT RUNTIME ERROR : luabind::cast_failed in function %s (%s)!",#v_func_name,#ret_type);\
-			return ((ret_type)(0));													\
-		}
-#else
-#	define CAST_FAILED(v_func_name,ret_type)
-#endif
 
 #define DEFINE_LUA_WRAPPER_CONST_METHOD_0(v_func_name,ret_type)							\
 		virtual ret_type v_func_name() const											\
 		{																				\
-			try {																		\
 				return luabind::call_member<ret_type>(this,#v_func_name);						\
-			}																			\
-			CAST_FAILED(v_func_name,ret_type)											\
-			catch(...) {																\
-				return ((ret_type)(0));													\
-			}																			\
 		}																				\
 		static ret_type v_func_name##_static(const inherited* ptr)						\
 		{                                                                               \
 			return ptr->self_type::inherited::v_func_name();							\
 		}
 
-#ifdef DEBUG
-#define DEFINE_LUA_WRAPPER_CONST_METHOD_1(v_func_name,ret_type,t1)						\
-		virtual ret_type v_func_name(t1 p1) const										\
-		{																				\
-			try {																		\
-				return luabind::call_member<ret_type>(this,#v_func_name,p1);						\
-			}																			\
-			CAST_FAILED(v_func_name,ret_type)											\
-			catch(...) {																\
-				return ((ret_type)(0));													\
-			}																			\
-		}																				\
-		static ret_type v_func_name##_static(const inherited* ptr, t1 p1)				\
-		{                                                                               \
-			return ptr->self_type::inherited::v_func_name(p1);							\
-		}
-#else // DEBUG
 #define DEFINE_LUA_WRAPPER_CONST_METHOD_1(v_func_name,ret_type,t1)						\
 		virtual ret_type v_func_name(t1 p1) const										\
 		{																				\
@@ -81,16 +49,11 @@
 		{                                                                               \
 			return ptr->self_type::inherited::v_func_name(p1);							\
 		}
-#endif // DEBUG
 
 #define DEFINE_LUA_WRAPPER_METHOD_V0(v_func_name)										\
 		virtual void v_func_name()														\
 		{																				\
-			try {																		\
 				luabind::call_member<void>(this,#v_func_name);									\
-			}																			\
-			catch(...) {																\
-			}																			\
 		}																				\
 		static void v_func_name##_static(inherited* ptr)								\
 		{                                                                               \
@@ -100,11 +63,7 @@
 #define DEFINE_LUA_WRAPPER_METHOD_V1(v_func_name,t1)									\
 		virtual void v_func_name(t1 p1)													\
 		{																				\
-			try {																		\
 				luabind::call_member<void>(this,#v_func_name,p1);								\
-			}																			\
-			catch(...) {																\
-			}																			\
 		}																				\
 		static void v_func_name##_static(inherited* ptr, t1 p1)							\
 		{                                                                               \
@@ -114,13 +73,7 @@
 #define DEFINE_LUA_WRAPPER_METHOD_0(v_func_name,ret_type)								\
 		virtual ret_type v_func_name()													\
 		{																				\
-			try {																		\
 				return luabind::call_member<ret_type>(this,#v_func_name);						\
-			}																			\
-			CAST_FAILED(v_func_name,ret_type)											\
-			catch(...) {																\
-				return ((ret_type)(0));													\
-			}																			\
 		}																				\
 		static ret_type v_func_name##_static(inherited* ptr)							\
 		{                                                                               \
@@ -130,29 +83,18 @@
 #define DEFINE_LUA_WRAPPER_METHOD_1(v_func_name,ret_type,t1)							\
 		virtual ret_type v_func_name(t1 p1)                                    			\
 		{																				\
-			try {																		\
 				return luabind::call_member<ret_type>(this,#v_func_name,p1);						\
-			}																			\
-			CAST_FAILED(v_func_name,ret_type)											\
-			catch(...) {																\
-				return ((ret_type)(0));													\
-			}																			\
 		}                                   											\
 		static  ret_type v_func_name##_static(inherited* ptr, t1 p1)            		\
 		{																				\
 			return ptr->self_type::inherited::v_func_name(p1);                          \
 		}
 
-//////////////////////////////////////////////////////////////////////////
 
 #define DEFINE_LUA_WRAPPER_METHOD_R2P1_V1(v_func_name,t1)								\
 		virtual void v_func_name(t1& p1)												\
 		{																				\
-			try {																		\
 				this->call<void>(#v_func_name,&p1);											\
-			}																			\
-			catch(...) {																\
-			}																			\
 		}                                   											\
 		static  void v_func_name##_static(inherited* ptr, t1* p1)						\
 		{																				\
@@ -162,11 +104,7 @@
 #define DEFINE_LUA_WRAPPER_METHOD_R2P1_V2(v_func_name,t1,t2)							\
 		virtual void v_func_name(t1& p1, t2 p2)											\
 		{																				\
-			try {																		\
 				this->call<void>(#v_func_name,&p1,p2);										\
-			}																			\
-			catch(...) {																\
-			}																			\
 		}                                   											\
 		static  void v_func_name##_static(inherited* ptr, t1* p1, t2 p2)				\
 		{																				\
