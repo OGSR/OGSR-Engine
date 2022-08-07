083611f0e1b795e3acd9ec0db2680ddfe4aa0b87
xrSimpodin <16670637+xrSimpodin@users.noreply.github.com>
2022-08-07 11:56:26 +0300

    Тут была ошибка, если слишком длинный путь к папке с игрой

--- ogsr_engine/xr_3da/XR_IOConsole.cpp
+++ ogsr_engine/xr_3da/XR_IOConsole.cpp
@@ -516,8 +516,8 @@ void CConsole::Execute(LPCSTR cmd)
 }
 void CConsole::ExecuteScript(LPCSTR N)
 {
-    string128 cmd;
-    strconcat(sizeof(cmd), cmd, "cfg_load ", N);
+    string_path cmd;
+    xr_strconcat(cmd, "cfg_load ", N);
     Execute(cmd);
 }
 
