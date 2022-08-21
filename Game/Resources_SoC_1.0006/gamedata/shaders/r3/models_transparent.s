edb3eee8cdd8431e833aec5ad4f748065139e66f
xrSimpodin <16670637+xrSimpodin@users.noreply.github.com>
2022-08-21 16:52:19 +0300

    Фикс тумана для шейдера `models\transparent`
    [skip ci]

---
+++ Game/Resources_SoC_1.0006/gamedata/shaders/r3/models_transparent.s
@@ -0,0 +1,25 @@
+function l_special  (shader, t_base, t_second, t_detail)
+  shader:begin  ("model_distort4glass","particle_distort")
+      : sorting   (3, true)
+      : blend     (true,blend.srcalpha,blend.invsrcalpha)
+      : zb        (true,false)
+      : fog       (false)
+      : distort   (true)
+	shader: dx10texture ("s_base", t_base)
+	shader: dx10texture ("s_distort", "pfx\\pfx_dist_glass")
+	shader: dx10sampler ("smp_linear")
+end
+
+function normal   (shader, t_base, t_second, t_detail)
+  shader:begin    ("model_env_lq","model_env_lq")
+      : fog       (true)
+      : zb        (true,false)
+      : blend     (true,blend.srcalpha,blend.invsrcalpha)
+      : aref      (true,0)
+      : sorting   (3,true)
+	shader: dx10texture ("s_base", t_base)
+	shader:	dx10texture ("s_env", "sky\\sky_5_cube")
+
+	shader: dx10sampler ("smp_base")
+	shader: dx10sampler ("smp_rtlinear")
+end
