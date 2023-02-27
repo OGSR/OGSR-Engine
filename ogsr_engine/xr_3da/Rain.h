38057e2df86e18359af7909a5a53b4b91622373b
Sergii T <5493488+joye-ramone@users.noreply.github.com>
2023-02-28 00:17:40 +0200

    Праметры движкого дождя в одном месте

--- ogsr_engine/xr_3da/Rain.h
+++ ogsr_engine/xr_3da/Rain.h
@@ -8,6 +8,27 @@
 
 #include "../xrcdb/xr_collide_defs.h"
 
+static const int max_desired_items = 2500;
+
+static const float source_radius = 12.5f;
+static const float source_offset = 40.f;
+
+static const float max_distance = source_offset * 1.25f;
+static const float sink_offset = -(max_distance - source_offset);
+
+static const float drop_length = 7.f;
+static const float drop_width = 0.40f;
+static const float drop_angle = 3.0f;
+static const float drop_max_angle = deg2rad(10.f);
+static const float drop_max_wind_vel = 20.0f;
+static const float drop_speed_min = 40.f;
+static const float drop_speed_max = 80.f;
+
+const int max_particles = 1000;
+const int particles_cache = 400;
+
+const float particles_time = .3f;
+
 // refs
 class ENGINE_API IRender_DetailModel;
 
