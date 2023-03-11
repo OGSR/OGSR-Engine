df38b62f8d514c1b96b34a62b5b21719f1496f3a
Sergii T <5493488+joye-ramone@users.noreply.github.com>
2023-03-11 12:26:29 +0200

    Что б префетчь объектов не вылетал на левых секциях. Пусть в лог просто пишет

--- ogsr_engine/xr_3da/IGame_ObjectPool.cpp
+++ ogsr_engine/xr_3da/IGame_ObjectPool.cpp
@@ -24,13 +24,18 @@ void IGame_ObjectPool::prefetch()
     Render->models_begin_prefetch1(true);
     for (const auto& item : sect.Ordered_Data)
     {
-        CLASS_ID CLS = pSettings->r_clsid(item.first.c_str(), "class");
-        p_count++;
-        CObject* pObject = (CObject*)NEW_INSTANCE(CLS);
-        pObject->Load(item.first.c_str());
-        pObject->reload(item.first.c_str());
-        VERIFY2(pObject->cNameSect().c_str(), item.first.c_str());
-        m_PrefetchObjects.push_back(pObject);
+        if (pSettings->section_exist(item.first.c_str()))
+        {
+            CLASS_ID CLS = pSettings->r_clsid(item.first.c_str(), "class");
+            p_count++;
+            CObject* pObject = (CObject*)NEW_INSTANCE(CLS);
+            pObject->Load(item.first.c_str());
+            pObject->reload(item.first.c_str());
+            VERIFY2(pObject->cNameSect().c_str(), item.first.c_str());
+            m_PrefetchObjects.push_back(pObject);
+        }
+        else
+            Msg("! [%s] unknown section %s in %s", __FUNCTION__, item.first.c_str(), section);
     }
     Render->models_begin_prefetch1(false);
     Msg("[%s] objects prefetching time (%zi): [%.2f s.]", __FUNCTION__, p_count, T.GetElapsed_sec());
@@ -60,79 +65,3 @@ CObject* IGame_ObjectPool::create(LPCSTR name)
 }
 
 void IGame_ObjectPool::destroy(CObject* O) { xr_delete(O); }
-
-/*
-void IGame_ObjectPool::prefetch	()
-{
-    R_ASSERT			(map_POOL.empty());
-
-    u32	mem_0			= Memory.mem_usage();
-    float	p_time		= 1000.f*Device.GetTimerGlobal()->GetElapsed_sec();
-    int	p_count			= 0;
-    ::Render->model_Logging	(FALSE);
-
-    string256 section;
-    // prefetch objects
-    strconcat				(section,"prefetch_objects_",g_pGamePersistent->m_game_params.m_game_type);
-    CInifile::Sect& sect	= pSettings->r_section(section);
-    for (CInifile::SectIt I=sect.begin(); I!=sect.end(); I++)	{
-        CInifile::Item& item= *I;
-        CLASS_ID CLS		= pSettings->r_clsid(item.first.c_str(),"class");
-        int count			= atoi(item.second.c_str());
-        count				+= (count==0)?1:0;
-        R_ASSERT2			((count>0) && (count<=128), "Too many objects for prefetching");
-        p_count				+=	count;
-        for (int c=0; c<count; c++){
-            CObject* pObject= (CObject*) NEW_INSTANCE(CLS);
-            pObject->Load	(item.first.c_str());
-            VERIFY2			(pObject->cNameSect().c_str(),item.first.c_str());
-            map_POOL.insert	(mk_pair(pObject->cNameSect(),pObject));
-        }
-    }
-
-    // out statistic
-    ::Render->model_Logging	(TRUE);
-    p_time = 1000.f*Device.GetTimerGlobal()->GetElapsed_sec() - p_time;
-    u32		p_mem		= Memory.mem_usage() - mem_0;
-    if (p_count){
-        float 	a_time		= float(p_time)/float(p_count);
-        Msg					("* [Object-prefetch] objects: %d",		p_count);
-        Msg					("* [Object-prefetch] time:    %d ms",	iFloor(p_time));
-        Msg					("* [Object-prefetch] memory:  %dKb",	p_mem/1024);
-        Msg					("* [Object-prefetch] average: %2.2f ms, %d bytes", a_time, p_mem/p_count);
-    }
-}
-
-void IGame_ObjectPool::clear	( )
-{
-    // Clear POOL
-    for (POOL_IT it=map_POOL.begin(); it!=map_POOL.end(); it++)
-        xr_delete	(it->second);
-    map_POOL.clear();
-}
-
-CObject*	IGame_ObjectPool::create			( LPCSTR	name	)
-{
-    string256			l_name;
-    POOL_IT	it			=	map_POOL.find	(shared_str(strlwr(strcpy_s(l_name,name))));
-    if (it!=map_POOL.end())
-    {
-        // Instance found
-        CObject* O			=	it->second;
-        map_POOL.erase		(it);
-        return				O;
-    } else {
-        // Create and load _new instance
-        // Msg				("! Uncached loading '%s'...",name);
-        CLASS_ID CLS		=	pSettings->r_clsid		(name,"class");
-        CObject* O			=	(CObject*) NEW_INSTANCE	(CLS);
-        O->Load				(name);
-        return				O;
-    }
-}
-
-void		IGame_ObjectPool::destroy			( CObject*	O		)
-{
-    map_POOL.insert			(mk_pair(O->cNameSect(),O));
-}
-*/
