c22ce15b4546ce819d6e4cdc24accb430534a352
xrSimpodin <16670637+xrSimpodin@users.noreply.github.com>
2023-04-06 20:13:22 +0300

    Может когда то пригодится CCC_Vector4

--- ogsr_engine/xr_3da/xr_ioc_cmd.h
+++ ogsr_engine/xr_3da/xr_ioc_cmd.h
@@ -411,3 +411,57 @@ public:
     CCC_LoadCFG_custom(LPCSTR cmd);
     virtual bool allow(LPCSTR cmd);
 };
+
+class CCC_Vector4 : public IConsole_Command
+{
+private:
+    Fvector4* value;
+    Fvector4 min, max;
+
+public:
+    CCC_Vector4(LPCSTR N, Fvector4* V, const Fvector4 _min, const Fvector4 _max) : IConsole_Command(N), value(V)
+    {
+        min.set(_min);
+        max.set(_max);
+    }
+    const Fvector4 GetValue() const { return *value; }
+    Fvector4* GetValuePtr() const { return value; }
+
+    virtual void Execute(LPCSTR args) override
+    {
+        Fvector4 v;
+        if (4 != sscanf(args, "%f,%f,%f,%f", &v.x, &v.y, &v.z, &v.w))
+        {
+            if (4 != sscanf(args, "(%f,%f,%f,%f)", &v.x, &v.y, &v.z, &v.w))
+            {
+                InvalidSyntax();
+                return;
+            }
+        }
+
+        if (v.x < min.x || v.y < min.y || v.z < min.z || v.w < min.w)
+        {
+            InvalidSyntax();
+            return;
+        }
+        if (v.x > max.x || v.y > max.y || v.z > max.z || v.w > max.w)
+        {
+            InvalidSyntax();
+            return;
+        }
+        value->set(v);
+    }
+
+    virtual void Status(TStatus& S) override { xr_sprintf(S, sizeof(S), "(%f, %f, %f, %f)", value->x, value->y, value->z, value->w); }
+
+    virtual void Info(TInfo& I) override { xr_sprintf(I, sizeof(I), "vector4 in range [%e,%e,%e,%e]-[%e,%e,%e,%e]", min.x, min.y, min.z, min.w, max.x, max.y, max.z, max.w); }
+
+    virtual void fill_tips(vecTips& tips, u32 mode) override
+    {
+        TStatus str;
+        xr_sprintf(str, sizeof(str), "(%e, %e, %e, %e) (current) [(%e,%e,%e,%e)-(%e,%e,%e,%e)]", value->x, value->y, value->z, value->w, min.x, min.y, min.z, min.w, max.x, max.y,
+                   max.z, max.w);
+        tips.push_back(str);
+        IConsole_Command::fill_tips(tips, mode);
+    }
+};
