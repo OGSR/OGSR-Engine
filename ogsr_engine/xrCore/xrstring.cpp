a971fbb09cb9e2a87d23cd06547ad3c3001113bd
Sergii T <5493488+joye-ramone@users.noreply.github.com>
2023-07-26 18:48:17 +0300

    реализация str_container openXRay наша уже испортилась )))

--- ogsr_engine/xrCore/xrstring.cpp
+++ ogsr_engine/xrCore/xrstring.cpp
@@ -4,140 +4,224 @@
 
 XRCORE_API extern str_container* g_pStringContainer = NULL;
 
-constexpr size_t HEADER = sizeof(str_value);
-
-str_value* str_container::dock(const char* value)
+struct str_container_impl
 {
-    if (0 == value)
-        return 0;
+    xrCriticalSection cs;
+    static constexpr size_t buffer_size = 1024u * 256u;
+    str_value* buffer[buffer_size];
+    int num_docs;
 
-    cs.Enter();
-
-    str_value* result = 0;
-
-    // calc len
-    u32 s_len = xr_strlen(value);
-    u32 s_len_with_zero = (u32)s_len + 1;
-
-    // setup find structure
-    string16 header;
-    str_value* sv = (str_value*)header;
-    sv->dwReference = 0;
-    sv->dwLength = s_len;
-    sv->dwCRC = crc32(value, s_len);
-
-    // search
-    cdb::iterator I = container.find(sv); // only integer compares :)
-    if (I != container.end())
+    str_container_impl()
     {
-        // something found - verify, it is exactly our string
-        cdb::iterator save = I;
-        for (; I != container.end() && (*I)->dwCRC == sv->dwCRC; ++I)
+        num_docs = 0;
+        ZeroMemory(buffer, sizeof(buffer));
+    }
+
+    str_value* find(str_value* value, const char* str) const
+    {
+        str_value* candidate = buffer[value->dwCRC % buffer_size];
+        while (candidate)
         {
-            str_value* V = (*I);
-            if (V->dwLength != sv->dwLength)
-                continue;
-            if (0 != memcmp(V->value, value, s_len))
-                continue;
-            result = V; // found
-            break;
+            if (candidate->dwCRC == value->dwCRC && candidate->dwLength == value->dwLength && !memcmp(candidate->value, str, value->dwLength))
+            {
+                return candidate;
+            }
+
+            candidate = candidate->next;
+        }
+
+        return nullptr;
+    }
+
+    void insert(str_value* value)
+    {
+        str_value** element = &buffer[value->dwCRC % buffer_size];
+        value->next = *element;
+        *element = value;
+    }
+
+    void clean()
+    {
+        for (size_t i = 0; i < buffer_size; ++i)
+        {
+            str_value** current = &buffer[i];
+
+            while (*current != nullptr)
+            {
+                str_value* value = *current;
+                if (!value->dwReference)
+                {
+                    *current = value->next;
+                    xr_free(value);
+                }
+                else
+                {
+                    current = &value->next;
+                }
+            }
         }
     }
 
-    // it may be the case, string is not fount or has "non-exact" match
-    if (0 == result)
+    void verify() const
     {
-        // Insert string
-        result = (str_value*)Memory.mem_alloc(HEADER + s_len_with_zero);
+        Msg("strings verify started");
+        for (size_t i = 0; i < buffer_size; ++i)
+        {
+            const str_value* value = buffer[i];
+            while (value)
+            {
+                const auto crc = crc32(value->value, value->dwLength);
+                string32 crc_str;
+                R_ASSERT(crc == value->dwCRC, "CorePanic: read-only memory corruption (shared_strings)", itoa(value->dwCRC, crc_str, 16));
+                R_ASSERT(value->dwLength == xr_strlen(value->value), "CorePanic: read-only memory corruption (shared_strings, internal structures)", value->value);
+                value = value->next;
+            }
+        }
+        Msg("strings verify completed");
+    }
+
+    void dump(FILE* f) const
+    {
+        for (size_t i = 0; i < buffer_size; ++i)
+        {
+            str_value* value = buffer[i];
+            while (value)
+            {
+                fprintf(f, "ref[%4u]-len[%3u]-crc[%8X] : %s\n", value->dwReference, value->dwLength, value->dwCRC, value->value);
+                value = value->next;
+            }
+        }
+    }
+
+    void dump(IWriter* f) const
+    {
+        for (size_t i = 0; i < buffer_size; ++i)
+        {
+            str_value* value = buffer[i];
+            string4096 temp;
+            while (value)
+            {
+                xr_sprintf(temp, sizeof(temp), "ref[%4u]-len[%3u]-crc[%8X] : %s\n", value->dwReference, value->dwLength, value->dwCRC, value->value);
+                f->w_string(temp);
+                value = value->next;
+            }
+        }
+    }
+
+    ptrdiff_t stat_economy() const
+    {
+        ptrdiff_t counter = 0;
+        for (size_t i = 0; i < buffer_size; ++i)
+        {
+            const str_value* value = buffer[i];
+            while (value)
+            {
+                counter -= sizeof(str_value);
+                counter += (value->dwReference - 1) * (value->dwLength + 1);
+                value = value->next;
+            }
+        }
+
+        return counter;
+    }
+};
+
+str_container::str_container() : impl(xr_new<str_container_impl>()) {}
+
+str_value* str_container::dock(pcstr value) const
+{
+    if (nullptr == value)
+        return nullptr;
+
+    impl->cs.Enter();
+
+    str_value* result = nullptr;
+
+    // calc len
+    const auto s_len = xr_strlen(value);
+    const auto s_len_with_zero = s_len + 1;
+    VERIFY(sizeof(str_value) + s_len_with_zero < 4096);
+
+    // setup find structure
+    char header[sizeof(str_value)];
+    str_value* sv = (str_value*)header;
+    sv->dwReference = 0;
+    sv->dwLength = static_cast<u32>(s_len);
+    sv->dwCRC = crc32(value, s_len);
+
+    // search
+    result = impl->find(sv, value);
+
+#ifdef DEBUG
+    const bool is_leaked_string = !xr_strcmp(value, "enter leaked string here");
+#endif // DEBUG
+
+    // it may be the case, string is not found or has "non-exact" match
+    if (nullptr == result
+#ifdef DEBUG
+        || is_leaked_string
+#endif // DEBUG
+    )
+    {
+        result = static_cast<str_value*>(xr_malloc(sizeof(str_value) + s_len_with_zero));
+
+#ifdef DEBUG
+        static int num_leaked_string = 0;
+        if (is_leaked_string)
+        {
+            ++num_leaked_string;
+            Msg("leaked_string: %d 0x%08x", num_leaked_string, result);
+        }
+#endif // DEBUG
 
         result->dwReference = 0;
         result->dwLength = sv->dwLength;
         result->dwCRC = sv->dwCRC;
         CopyMemory(result->value, value, s_len_with_zero);
-        container.insert(result);
-    }
 
-    cs.Leave();
+        impl->insert(result);
+    }
+    impl->cs.Leave();
 
     return result;
 }
 
-void str_container::clean()
+void str_container::clean() const
 {
-    cs.Enter();
-    cdb::iterator it = container.begin();
-    cdb::iterator end = container.end();
-    for (; it != end;)
-    {
-        str_value* sv = *it;
-        if (0 == sv->dwReference)
-        {
-            cdb::iterator i_current = it;
-            cdb::iterator i_next = ++it;
-            xr_free(sv);
-            container.erase(i_current);
-            it = i_next;
-        }
-        else
-        {
-            it++;
-        }
-    }
-    if (container.empty())
-        container.clear();
-    cs.Leave();
+    impl->cs.Enter();
+    impl->clean();
+    impl->cs.Leave();
 }
 
-void str_container::verify()
+void str_container::verify() const
 {
-    cs.Enter();
-    cdb::iterator it = container.begin();
-    cdb::iterator end = container.end();
-    for (; it != end; ++it)
-    {
-        str_value* sv = *it;
-        u32 crc = crc32(sv->value, sv->dwLength);
-        string32 crc_str;
-        R_ASSERT3(crc == sv->dwCRC, "CorePanic: read-only memory corruption (shared_strings)", itoa(sv->dwCRC, crc_str, 16));
-        R_ASSERT3(sv->dwLength == xr_strlen(sv->value), "CorePanic: read-only memory corruption (shared_strings, internal structures)", sv->value);
-    }
-    cs.Leave();
+    impl->cs.Enter();
+    impl->verify();
+    impl->cs.Leave();
 }
 
-void str_container::dump()
+void str_container::dump() const
 {
-    cs.Enter();
-    cdb::iterator it = container.begin();
-    cdb::iterator end = container.end();
-    FILE* F = fopen("x:\\$str_dump$.txt", "w");
-    for (; it != end; it++)
-        fprintf(F, "ref[%4u]-len[%3u]-crc[%8X] : %s\n", (*it)->dwReference, (*it)->dwLength, (*it)->dwCRC, (*it)->value);
+    impl->cs.Enter();
+    FILE* F = fopen("d:\\$str_dump$.txt", "w");
+    impl->dump(F);
     fclose(F);
-    cs.Leave();
+    impl->cs.Leave();
 }
 
-u32 str_container::stat_economy()
+size_t str_container::stat_economy() const
 {
-    cs.Enter();
-    cdb::iterator it = container.begin();
-    cdb::iterator end = container.end();
-    int counter = 0;
+    impl->cs.Enter();
+    ptrdiff_t counter = 0;
     counter -= sizeof(*this);
-    counter -= sizeof(cdb::allocator_type);
-    const int node_size = 20;
-    for (; it != end; it++)
-    {
-        counter -= HEADER;
-        counter -= node_size;
-        counter += int((int((*it)->dwReference) - 1) * int((*it)->dwLength + 1));
-    }
-    cs.Leave();
-
-    return u32(counter);
+    counter += impl->stat_economy();
+    impl->cs.Leave();
+    return size_t(counter);
 }
 
 str_container::~str_container()
 {
     clean();
-    // R_ASSERT(container.empty());
+    // dump ();
+    xr_delete(impl);
 }
--- ogsr_engine/xrCore/xrstring.h
+++ ogsr_engine/xrCore/xrstring.h
@@ -9,33 +9,32 @@ struct XRCORE_API str_value
     u32 dwReference;
     u32 dwLength;
     u32 dwCRC;
+    str_value* next;
     char value[];
 };
-struct XRCORE_API str_value_cmp
-{ // less
-    IC bool operator()(const str_value* A, const str_value* B) const { return A->dwCRC < B->dwCRC; };
-};
+
 #pragma warning(default : 4200)
+
+struct str_container_impl;
 
 //////////////////////////////////////////////////////////////////////////
 class XRCORE_API str_container
 {
-private:
-    typedef xr_multiset<str_value*, str_value_cmp> cdb;
-    xrCriticalSection cs;
-    cdb container;
-
 public:
-    str_value* dock(const char* value);
-    void clean();
-    void dump();
-    void verify();
-    u32 stat_economy();
-#ifdef PROFILE_CRITICAL_SECTIONS
-    str_container() : cs(MUTEX_PROFILE_ID(str_container)) {}
-#endif // PROFILE_CRITICAL_SECTIONS
+    str_container();
     ~str_container();
+
+    str_value* dock(pcstr value) const;
+    void clean() const;
+    void dump() const;
+    void verify() const;
+
+    [[nodiscard]] size_t stat_economy() const;
+
+private:
+    str_container_impl* impl;
 };
+
 XRCORE_API extern str_container* g_pStringContainer;
 
 //////////////////////////////////////////////////////////////////////////
@@ -114,10 +113,12 @@ public:
     char operator[](size_t id) const { return p_->value[id]; }
     bool operator!() const { return !p_; }
     const char* operator*() const { return p_ ? p_->value : nullptr; }
+
     // Чтобы можно было легко кастить в std::string_view как и все остальные строки
     operator std::string_view() const { return std::string_view{p_ ? p_->value : ""}; }
 
     const char* c_str() const { return p_ ? p_->value : nullptr; }
+
     //Используется в погодном редакторе.
     const char* data() const { return p_ ? p_->value : ""; }
 
@@ -150,6 +151,7 @@ public:
 
 // externally visible standart functionality
 IC void swap(shared_str& lhs, shared_str& rhs) { lhs.swap(rhs); }
+
 IC u32 xr_strlen(shared_str& a) { return a.size(); }
 IC int xr_strcmp(const shared_str& a, const char* b) { return xr_strcmp(*a, b); }
 IC int xr_strcmp(const char* a, const shared_str& b) { return xr_strcmp(a, *b); }
@@ -160,6 +162,7 @@ IC int xr_strcmp(const shared_str& a, const shared_str& b)
     else
         return xr_strcmp(*a, *b);
 }
+
 IC void xr_strlwr(xr_string& src)
 {
     for (xr_string::iterator it = src.begin(); it != src.end(); it++)
@@ -198,7 +201,6 @@ struct transparent_string_equal
 
 template <typename Key, typename Value, class _Alloc = std::allocator<std::pair<const Key, Value>>>
 using string_unordered_map = std::unordered_map<Key, Value, transparent_string_hash, transparent_string_equal, _Alloc>;
-
 
 inline bool SplitFilename(std::string& str)
 {
