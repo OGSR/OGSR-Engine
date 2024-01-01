c5e77159904beac601011275390415ce3b9b888b
xrSimpodin <16670637+xrSimpodin@users.noreply.github.com>
2024-01-01 14:29:06 +0300

    Обновление до последних версий

--- Update_DirectX_Components.cmd
+++ Update_DirectX_Components.cmd
@@ -1,8 +1,8 @@
 RD /s /q 3rd_party\Src\DirectXTex\DirectXTex
-git clone --branch oct2023 --depth 1 https://github.com/microsoft/DirectXTex.git 3rd_party/Src/DirectXTex/DirectXTex
+git clone --branch dec2023 --depth 1 https://github.com/microsoft/DirectXTex.git 3rd_party/Src/DirectXTex/DirectXTex
 
 RD /s /q 3rd_party\Src\DirectXMesh\DirectXMesh
-git clone --branch oct2023 --depth 1 https://github.com/microsoft/DirectXMesh.git 3rd_party/Src/DirectXMesh/DirectXMesh
+git clone --branch dec2023 --depth 1 https://github.com/microsoft/DirectXMesh.git 3rd_party/Src/DirectXMesh/DirectXMesh
 
 RD /s /q 3rd_party\Src\DirectXMath\DirectXMath
-git clone --branch dec2022 --depth 1 https://github.com/microsoft/DirectXMath.git 3rd_party/Src/DirectXMath/DirectXMath
+git clone --branch dec2023 --depth 1 https://github.com/microsoft/DirectXMath.git 3rd_party/Src/DirectXMath/DirectXMath
