2f0495944fe9d4c7fbef7b456d295508596b3e49
JR <5493488+joye-ramone@users.noreply.github.com>
2024-02-25 15:28:00 +0200

    не нужен тут в condition_factor этот powf

--- ogsr_engine/xrGame/trade2.cpp
+++ ogsr_engine/xrGame/trade2.cpp
@@ -140,7 +140,7 @@ u32 CTrade::GetItemPrice(PIItem pItem, bool b_buying)
 
     // computing condition factor
     // for "dead" weapon we use 10% from base cost, for "good" weapon we use full base cost
-    float condition_factor = powf(pItem->GetCondition() * 0.9f + .1f, 0.75f);
+    const float condition_factor = 0.1f + pItem->GetCondition() * 0.9f; // powf(pItem->GetCondition() * 0.9f + .1f, 0.75f);
 
     // computing relation factor
     float relation_factor;
