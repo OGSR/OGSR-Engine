b08795b1c14113f829febf4ec3c04fd4d3c911e6
PRXPHET <75882664+PRXPHET@users.noreply.github.com>
2025-01-28 15:32:33 +0300

    Фикс мыльной карты в ПДА
    Co-Authored-By: JR <5493488+joye-ramone@users.noreply.github.com>

--- ogsr_engine/xrGame/ui/UIEventsWnd.cpp
+++ ogsr_engine/xrGame/ui/UIEventsWnd.cpp
@@ -187,8 +187,17 @@ void CUIEventsWnd::ReloadList(bool bClearOnly)
 void CUIEventsWnd::Show(bool status)
 {
     inherited::Show(status);
+
+    // хз почему, но если сделать Show для m_UIMapWnd, который не приаттачен к родителю - на нём пропадают LevelMap, причину не нашел, будет пока костыль
+
+    if (!GetDescriptionMode())
+        m_UIRightWnd->AttachChild(m_UIMapWnd);
+
     m_UIMapWnd->Show(status);
     m_UITaskInfoWnd->Show(status);
+
+    if (!GetDescriptionMode())
+        m_UIRightWnd->DetachChild(m_UIMapWnd);
 
     ReloadList(status == false);
 }
